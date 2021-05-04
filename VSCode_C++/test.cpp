1dc1a55f06391edb992d69262d913b6f6b5e678a
Bless-Han <327496836@qq.com>
2021-05-04 12:46:25 +0800

    Started product

---
+++ VSCode_C++/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello World!!\n";
+    int i;
+    cin >> i;
+    for (int j = 0; j < i; ++j)
+        cout << j << endl;
+    return 0;
+}

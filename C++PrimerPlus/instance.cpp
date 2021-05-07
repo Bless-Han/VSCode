a14d80bc88946044af39a7796e532cc0878d0e78
Bless-Han <327496836@qq.com>
2021-05-07 09:49:32 +0800

    main

--- C++PrimerPlus/instance.cpp
+++ C++PrimerPlus/instance.cpp
@@ -21,6 +21,7 @@ long double probability(unsigned numbers, unsigned picks){
   long double n;
   unsigned p;
 
+  
   for (n = numbers, p = picks; p > 0; n--, p--)
     result = result * n / p;
   return result;

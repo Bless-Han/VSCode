dcdd800d30a573382a0b85f8ac5067bbf2d96c56
Bless-Han <327496836@qq.com>
2023-01-08 15:17:56 +0800

    origin/main

--- test.c
+++ test.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <cs50.h>
 
+typedef struct
+{
+    string name;
+}
+item;
+
+item items[5];
 int main()
 {
-    printf("hello world\n");
+    items[0].name = "good";
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s, bool is %i\n", items[i].name, items[i].name == NULL);
+    }
+    
 }

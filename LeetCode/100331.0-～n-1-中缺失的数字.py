ad62f8a120728e7b216bc4baf391c8b188ce0ec5
Bless-Han <327496836@qq.com>
2023-01-08 11:04:17 +0800

    origin/main

---
+++ LeetCode/100331.0-～n-1-中缺失的数字.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=100331 lang=python3
+#
+# [剑指 Offer 53 - II] 0～n-1中缺失的数字
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+# @lc code=end
+
---
+++ LeetCode/100333.二叉搜索树的第k大节点.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=100333 lang=python3
+#
+# [剑指 Offer 54] 二叉搜索树的第k大节点
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+        
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        def dfs(root):
+        return self.res
+
+# @lc code=end
+

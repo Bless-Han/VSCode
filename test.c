0e28d341b138fe8e450233bc7707fb4bb165cbbc
Bless-Han <327496836@qq.com>
2023-01-08 10:03:43 +0800

    origin/main

--- test.c
+++ test.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void draw(int n);
+
 int main()
 {
-    int i = get_int("Enter a number: ");
-    printf("%i i\n", i);
+    int n = get_int("Enter a number: ");
+    draw(n);
+    
+}
+
+void draw(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    
+    draw(n - 1);
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }

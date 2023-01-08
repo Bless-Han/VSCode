447d3b8e93c34d9302376ec02b6d855370241210
Bless-Han <327496836@qq.com>
2023-01-08 20:11:45 +0800

    origin/main

--- test.c
+++ test.c
@@ -10,10 +10,9 @@ item;
 item items[5];
 int main()
 {
-    items[0].name = "good";
+    items[].name = "good";
     for (int i = 0; i < 5; i++)
     {
         printf("%s, bool is %i\n", items[i].name, items[i].name == NULL);
     }
-    
 }

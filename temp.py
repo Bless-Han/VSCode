19eb0bc21408a1f7a942519b9211224d787e5d1e
Bless-Han <327496836@qq.com>
2023-01-09 21:02:04 +0800

    origin/main

--- temp.py
+++ temp.py
@@ -1,2 +1,6 @@
-name = input("Enter your name: ")
-print("hello,",name)
+print("Welcome to the Band Name Generator.")
+print("What's name of the city you grew in?")
+city = input();
+print("What's your pet's name?")
+pet = input();
+print("Your band name could be", city, pet)

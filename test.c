c34fd7de78275fe221f5b8a8f2bf46411ee70b14
Bless-Han <327496836@qq.com>
2023-01-12 17:49:23 +0800

    origin/main

--- test.c
+++ test.c
@@ -2,17 +2,10 @@
 #include <cs50.h>
 
 
-typedef struct
-{
-    int i;
-}
-item;
 
-item items[5];
 int main()
 {
-    int i = 9;
-    item test;
-    test.i = 100;
-    printf("%i i, %i test.i\n", i, test.i);
+    char *s = "HI!";
+    char *p = s;
+    printf("%s\n", p);
 }

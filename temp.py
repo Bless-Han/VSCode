b7fb6f739749533defb7a684be7924f36bef235c
Bless-Han <327496836@qq.com>
2023-01-14 20:47:10 +0800

    origin/main

--- temp.py
+++ temp.py
@@ -1,5 +1,15 @@
-bill_per_person = 8 / 3
-final = "{:.2f}".format(bill_per_person);
-
-print(type(final))
-print(final)
+def add(a, b):
+    print(a + b)
+a = 2
+if a == 0:
+    print("Yes")
+elif a == 9:
+    print("Yes 9")
+else:
+    print("No")
+    
+print(f"Your BMI is {bmi}, you are underweight.")
+print(f"Your BMI is {bmi}, you have a normal weight.")
+print(f"Your BMI is {bmi}, you are slightly overweight.")
+print(f"Your BMI is {bmi}, you are obese.")
+print(f"Your BMI is {bmi}, you are clinically obese.")

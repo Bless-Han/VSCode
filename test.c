84c0e0a8d79b3cbdba2a25aa6345c4f0298657b0
Bless-Han <327496836@qq.com>
2023-01-17 15:14:31 +0800

    origin/main

--- test.c
+++ test.c
@@ -4,20 +4,11 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-typedef int integer;
-
 int main()
 {
-    int *x;
-    int *y;
-    x = malloc(sizeof(int));
-    
-
-    *x = 42;
-    printf("%i x\n", *x);
-
-    y = x;
-    *y = 13;
-    printf("%i x\n", *x);
-    free(x);
+    // char *s = malloc(20 * sizeof(char));
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
 }

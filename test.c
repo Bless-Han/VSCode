857cc5c3455c2a73cce6c937b43b74fd7b585137
Bless-Han <327496836@qq.com>
2023-01-22 18:43:23 +0800

    origin/main

--- test.c
+++ test.c
@@ -8,8 +8,8 @@ typedef struct
 
 int main()
 {
-    two_number m[2];
-    m[0].a = 20;
-    m[1] = m[0];
-    printf("m: %i %p \nm: %i %p\n", m[0].a, &m[0], m[1].a, &m[1]);
+    int a = -2;
+    for ( ; a < 0; a++);
+    printf("a: %i\n", a);
+    
 }

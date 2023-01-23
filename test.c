8640c816bcdc323eab849f5b622832a1dd8e00bb
Bless-Han <327496836@qq.com>
2023-01-23 10:03:31 +0800

    origin/main

--- test.c
+++ test.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 
-typedef struct 
-{
-    int a;
-    int b;
-} two_number;
-
 int main()
 {
-    char s[20];
-    for (int i = 0; i < 20; i++)
-    {
-        /* code */
-        sprintf(s, "%03i", i);
-        s[2] = '\0';
-        printf("s: %s\n", s);
-    }
 }

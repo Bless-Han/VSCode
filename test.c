0976d59bff5abd23cc3ceb78f574c2ee44f47a8a
Bless-Han <327496836@qq.com>
2023-01-24 12:54:33 +0800

    origin/main

--- test.c
+++ test.c
@@ -8,8 +8,36 @@ typedef struct node
 }
 node;
 
-int main()
+int main(int argc, char *argv[])
 {
-    char *s = "200";
-    printf("s: %i\n", atoi(s));
+    node *list = NULL;
+    node *tmp = NULL;
+    // ini
+    for (int i = 1; i < argc; i++)
+    {
+        tmp = malloc(sizeof(node));
+        if (tmp == NULL)
+        {
+            return 1;
+        }
+        tmp->number = atoi(argv[i]);
+        tmp->next = list;
+        list = tmp;
+    }
+    
+    node *ptr = list;
+    // print
+    for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%i\n", ptr->number);
+    }
+    
+    // free list
+    while (ptr != NULL)
+    {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+    }
+    return 0;
 }

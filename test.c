82846363f955f1fbce42764c951dcb2197eae30e
Bless-Han <327496836@qq.com>
2023-01-25 11:52:55 +0800

    origin/main

--- test.c
+++ test.c
@@ -1,43 +1,16 @@
 #include <stdio.h>
+#include <cs50.h>
 #include <stdlib.h>
 
 typedef struct node
 {
     int number;
-    struct node *next;
+    struct node *left;
+    struct node *right;
 }
 node;
 
 int main(int argc, char *argv[])
 {
-    node *list = NULL;
-    node *tmp = NULL;
-    // ini
-    for (int i = 1; i < argc; i++)
-    {
-        tmp = malloc(sizeof(node));
-        if (tmp == NULL)
-        {
-            return 1;
-        }
-        tmp->number = atoi(argv[i]);
-        tmp->next = list;
-        list = tmp;
-    }
-    
-    node *ptr = list;
-    // print
-    for (node *ptr = list; ptr != NULL; ptr = ptr->next)
-    {
-        printf("%i\n", ptr->number);
-    }
-    
-    // free list
-    while (ptr != NULL)
-    {
-        node *next = ptr->next;
-        free(ptr);
-        ptr = next;
-    }
-    return 0;
+    printf("%lu\n", sizeof(node));
 }

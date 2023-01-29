e0385256c1c492030dc9172b4ece1f2c5c8e9b68
Bless-Han <327496836@qq.com>
2023-01-29 09:46:34 +0800

    origin/main

--- test.c
+++ test.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
-
-#define N 90
+#include <ctype.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
-    FILE *file = fopen("test.txt", "r");
-    if (file == NULL)
-    {
-        printf("ERROR\n");
-        return 1;
-    }
-    char words[10][N];
-    for (int i = 0; fgets(words[i], N, file); i++)
-    {
-        printf("i: %i\n", i);
-    }
-    for(int i = 0; i < 10; i++)
-    {
-        printf("i: %i, s: %s\n", i, words[i]);
-    }
-
+    printf("%i\n", isalpha('A'));
 }

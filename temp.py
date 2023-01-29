4479eef0352838bfc56d2398d8e4a54ab8674eac
Bless-Han <327496836@qq.com>
2023-01-29 13:46:50 +0800

    origin/main

--- temp.py
+++ temp.py
@@ -1,2 +1,9 @@
-while True:
-    print("Neo")
+x = int(input("x: "))
+y = int(input("y: "))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")

463aebb1f429d96b52baed045b40e0f301b37239
Bless-Han <327496836@qq.com>
2023-02-01 21:21:58 +0800

    origin/main

--- temp.py
+++ temp.py
@@ -1,4 +1,27 @@
-teams = {"name": "China", "value": "200"}
-# teams["value"] = int(teams["value"])
+dic = {
+    "S": 0,
+    "t": 0,
+    "r": 0,
+    "i": 0,
+    "n": 0,
+    "g": 0,
+    "g": 0,
+    "k": "c",
+}
 
-print(type(teams["value"]))
+def main():
+    s = input()
+    print(dic)
+    init(s)
+    print_count()
+    
+def init(s):
+    for c in s:
+        if c in dic:
+            # print(c)
+            j = 0
+            
+def print_count():
+    i = 0
+
+main()

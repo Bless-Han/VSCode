14419406ec81b272c0062507300b3fec265d0cdf
Bless-Han <327496836@qq.com>
2023-02-08 21:25:28 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1026 程序运行时间.3.py
+++ pintia/PAT (Basic Level) Practice （中文）/1026 程序运行时间.3.py
@@ -10,7 +10,26 @@
 '''
 
 # @pintia code=start
+# change seconds to time(xx:xx:xx)
+def change(seconds):
+    minutes = 0
+    hours = 0
+    local_seconds = round(seconds % 60 + 0.0001)
+    seconds //= 60
+    if seconds > 0:
+        minutes = round(seconds % 60 + 0.0001)
+        seconds //= 60
+    if seconds > 0:
+        hours = round(seconds % 60 + 0.0001)
+    return f"{hours:02}:{minutes:02}:{local_seconds:02}"
+    
+   
 
+
+
+s = input().split()
+c1, c2 = map(int, s)
+print(change((c2 - c1) / 100))
 
 
 # @pintia code=end
---
+++ pintia/PAT (Basic Level) Practice （中文）/1041 考试座位号.3.py
@@ -0,0 +1,20 @@
+'''
+   @pintia psid=994805260223102976 pid=994805281567916032 compiler=PYTHON3
+   
+   ProblemSet: PAT (Basic Level) Practice （中文）
+   
+   Title: 1041 考试座位号
+   
+   https://pintia.cn/problem-sets/994805260223102976/problems/994805281567916032
+   
+'''
+
+# @pintia code=start
+# students like this ---- [{"card_id": "xxxxxx", "try_id": xxx, "examin_id": xxx}]
+students = []
+
+
+
+
+
+# @pintia code=end

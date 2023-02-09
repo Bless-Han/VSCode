30d9ce6ade76ebfc76044dd6b880da9fb87daf10
Bless-Han <327496836@qq.com>
2023-02-10 00:22:46 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -1,10 +1,4 @@
-from decimal import Decimal
-from decimal import ROUND_HALF_UP
+times = lambda a, b: a * b
 
-l = [1.4, 0.5, 2.5,]
 
-def hello(to="world"):
-    print(f"hello {to}")
-    
-hello()
-hello("Neo")
+print(times(2, 9))

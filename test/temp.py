b657dcdc469922af241ba8c26d0617810c92e06f
Bless-Han <327496836@qq.com>
2023-02-11 19:34:36 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -1,3 +1,10 @@
-a = [1, 2, 3, 4]
-for i, v in enumerate(a):
-	print(i, v)
+from decimal import Decimal
+from decimal import ROUND_HALF_UP
+a = [1.4, 0.5, 2.5]
+
+for i in a:
+	print(round(i + 0.00001))
+
+print("-" * 20)
+for i in a:
+	print(Decimal(i).quantize(Decimal("0"), ROUND_HALF_UP))

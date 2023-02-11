edca825e74f04fe138c6e82dbea05d4c07be750e
Bless-Han <327496836@qq.com>
2023-02-11 22:12:00 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -1,10 +1,3 @@
-from decimal import Decimal
-from decimal import ROUND_HALF_UP
-a = [1.4, 0.5, 2.5]
-
-for i in a:
-	print(round(i + 0.00001))
-
-print("-" * 20)
-for i in a:
-	print(Decimal(i).quantize(Decimal("0"), ROUND_HALF_UP))
+s = input().split()
+a, b = map(int, s)
+print(a + b)

10a966e4e97e0abeb72ea3a46ef3eb3106bf61e2
Bless-Han <327496836@qq.com>
2023-02-11 16:34:23 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1096 大美数.3.py
+++ pintia/PAT (Basic Level) Practice （中文）/1096 大美数.3.py
@@ -23,34 +23,7 @@ def main():
 
 
 def judge(number):
-    yinzi = []
-    
-    for i in range(1, int(number) + 1):
-        if number % i == 0:
-            yinzi.append(i)
-    # print(yinzi)
-    
-    if len(yinzi) < 4:
-        return False
-
-    sums = []
-    for i1, v1 in enumerate(yinzi):
-        for i2, v2 in enumerate(yinzi[i1+1:]):
-            for i3, v3 in enumerate(yinzi[i2+1:]):
-                for i4, v4 in enumerate(yinzi[i3+1:]):
-                    if number == v1 + v2 + v3 + v4:
-                        # print(v1, v2, v3, v4, v1 + v2 + v3 + v4)
-                        return True
-                    # sums.append(v1 + v2 + v3 + v4)
-    
-    # print(sums)
-    # for sm in sums:
-    #     if number % sm == 0:
-            # return True
-
-    return False
-    # TODO 去重
-    # TODO 排序
+    ...
 
 
 
@@ -58,5 +31,5 @@ main()
 # @pintia code=end
 """ @pintia test=start
 1
-36
+900
 @pintia test=end """
--- test/temp.py
+++ test/temp.py
@@ -1,3 +1,8 @@
-a = [1, 3, 6, 9]
-for i in enumerate(a[1:]):
-	print(i)
+a = [1, 2, 3, 4]
+for i1, v1 in enumerate(a):
+	for i2, v2 in enumerate(a[i1 + 1:]):
+		for i3, v3 in enumerate(a[i2 + 2:]):
+			print("i1:", v1)
+			print("i2:", v2)
+			print("i3:", v3)
+		print("-" * 20)

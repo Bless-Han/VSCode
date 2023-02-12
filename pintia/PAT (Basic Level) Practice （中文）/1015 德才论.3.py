2485776103ccd33467f86c8121a620affd22033f
Bless-Han <327496836@qq.com>
2023-02-13 05:05:59 +0800

    origin/main

---
+++ pintia/PAT (Basic Level) Practice （中文）/1015 德才论.3.py
@@ -0,0 +1,17 @@
+'''
+   @pintia psid=994805260223102976 pid=994805307551629312 compiler=PYTHON3
+   
+   ProblemSet: PAT (Basic Level) Practice （中文）
+   
+   Title: 1015 德才论
+   
+   https://pintia.cn/problem-sets/994805260223102976/problems/994805307551629312
+   
+'''
+
+# @pintia code=start
+def main():
+    ...
+
+main()
+# @pintia code=end

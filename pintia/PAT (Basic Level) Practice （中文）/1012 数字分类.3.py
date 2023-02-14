8b55af1ca6e764c1e76a86f04c08052eecf66d32
Bless-Han <327496836@qq.com>
2023-02-14 13:40:16 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1012 数字分类.3.py
+++ pintia/PAT (Basic Level) Practice （中文）/1012 数字分类.3.py
@@ -19,17 +19,47 @@ a5 = "N"
 s = input().split()
 numbers = list(map(int, s[1:]))
 
+# for number % == 1
+jiaocuo = 1
+
 for number in numbers:
-    ...
+    match number % 5:
+        case 0:
+            if number % 2 == 0:
+                if a1 == "N":
+                    a1 = number
+                else:  
+                    a1 += number
+        case 1:
+            if a2 == "N":
+                a2 = number
+            else:
+                a2 += number * jiaocuo
+            jiaocuo *= -1
+        case 2:
+            if a3 == "N":
+                a3 = 1
+            else:
+                a3 += 1
+        case 3:
+            if a4 == "N":
+                count = 1
+                sum = number
+                a4 = ""
+            else:
+                count += 1
+                sum += number
+        case 4:
+            if a5 == "N":
+                a5 = number
+            elif number > a5:
+                a5 = number
+
+if a4 != "N":
+    a4 = round(sum / count + 0.001, 1)
 
 
-print(a1, a2, a3, end=" ")
-if a4 == "N":
-    print(a4, end=" ")
-else:
-    print(f"{a4:.1f}", end=" ")
-print(a5)
 
-    
+print(a1, a2, a3, a4, a5)
 
 # @pintia code=end
---
+++ pintia/PAT (Basic Level) Practice （中文）/1013 数素数.3.py
@@ -0,0 +1,27 @@
+'''
+   @pintia psid=994805260223102976 pid=994805309963354112 compiler=PYTHON3
+   
+   ProblemSet: PAT (Basic Level) Practice （中文）
+   
+   Title: 1013 数素数
+   
+   https://pintia.cn/problem-sets/994805260223102976/problems/994805309963354112
+   
+'''
+
+# @pintia code=start
+def main():
+    m, n = map(int, input().split())
+    primes = 2;
+    for i in range()
+
+
+def is_prime(number):
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+main()
+# @pintia code=end

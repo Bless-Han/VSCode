df300d8e6488cdf965d34450f4c0cef61bdaddf3
Bless-Han <327496836@qq.com>
2023-02-16 19:34:40 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1003 我要通过！.3.py
+++ pintia/PAT (Basic Level) Practice （中文）/1003 我要通过！.3.py
@@ -10,11 +10,28 @@
 '''
 
 # @pintia code=start
+def full_A(s):
+    for c in s:
+        if c != "A":
+            return False
+
+    return True
+
+
 def judge(s):
     p_index = s.find("P")
     t_index = s.rfind("T")
-    left_count = s[]
+    p_count = s.count("P")
+    t_count = s.count("T")
+    if p_count != 1 or t_count != 1:
+        return False
+    left = s[:p_index]
+    middle = s[p_index + 1: t_index]
+    right = s[t_index + 1:]
+    if full_A(left) == False or full_A(middle) == False or full_A(right) == False:
+        return False
 
+    return len(left) * len(middle) == len(right) and len(middle) > 0
 
 
 n = int(input())

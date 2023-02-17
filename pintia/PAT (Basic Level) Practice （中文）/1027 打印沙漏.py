54f9ce0cd269e0d76b153ba10e72d7cd5188033e
Bless-Han <327496836@qq.com>
2023-02-17 19:50:59 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1027 打印沙漏.py
+++ pintia/PAT (Basic Level) Practice （中文）/1027 打印沙漏.py
@@ -11,18 +11,33 @@
 # @pintia code=start
 n, c = input().split()
 n = int(n)
-# 计算一半的高度
+
 height = 1
 stars = 1
 while stars <= n:
     height += 1
-    stars += ((height * 2) - 1) * 2
+    stars += (height * 2 - 1) * 2
 height -= 1
 
+current_height = height
+# judge: 过半变-1
+judge = 1
+while True:
+    # 到中点
+    if current_height == 1:
+        judge = -1
 
+    current_stars = current_height * 2 - 1
+    print(" " * (height - current_height) + c * current_stars)
 
+    n -= current_stars
+    if current_height == height and judge == -1:
+        print(n)
+        break
+
+    current_height -= judge
 
 # @pintia code=end
 """ @pintia test=start
-3 $
+6 $
 @pintia test=end """

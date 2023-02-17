3c6c9de12e9268f8398ef6dc8451a082da838248
Bless-Han <327496836@qq.com>
2023-02-17 18:50:55 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1024 科学计数法.py
+++ pintia/PAT (Basic Level) Practice （中文）/1024 科学计数法.py
@@ -9,8 +9,37 @@
    
 '''
 # @pintia code=start
+left, right = input().split("E")
+result = ""
+if left[0] == "-":
+    result += "-"
+left = left[1:]
+
+# E 右边的那个数字
+right_number = int(right[0] + str(int(right[1:])))
+
+if right_number == 0:
+    result += left
+elif right_number < 0:
+    left = left.replace(".", "")
+    result += "0." + "0" * (abs(right_number) - 1) + left
+else:
+    left = left.replace(".", "")
+    # right_of_point：小数点右边的长度
+    right_of_point = len(left) - 1
+    if right_number >= right_of_point:
+        result += left + "0" * (right_number - right_of_point)
+    else:
+        # point_location 小数点新的位置
+        point_location = right_number + 1
+        left = list(left)
+        left.insert(point_location, ".")
+        result += "".join(left)
+        
+
+print(result)
 
 # @pintia code=end
 """ @pintia test=start
-+1.23400E+04
++1.23400E+05
 @pintia test=end """
--- pintia/PAT (Basic Level) Practice （中文）/1027 打印沙漏.py
+++ pintia/PAT (Basic Level) Practice （中文）/1027 打印沙漏.py
@@ -9,11 +9,20 @@
    
 '''
 # @pintia code=start
+n, c = input().split()
+n = int(n)
+# 计算一半的高度
+height = 1
+stars = 1
+while stars <= n:
+    height += 1
+    stars += ((height * 2) - 1) * 2
+height -= 1
 
-    
+
 
 
 # @pintia code=end
 """ @pintia test=start
-6 $
+3 $
 @pintia test=end """

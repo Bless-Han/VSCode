525ef3230f59ac2d804225c20af9938482e2b86f
Bless-Han <327496836@qq.com>
2023-02-18 10:04:21 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1034 有理数四则运算.py
+++ pintia/PAT (Basic Level) Practice （中文）/1034 有理数四则运算.py
@@ -20,24 +20,79 @@ def zdgy(a, b):
     return b
 
 
-def minus(a, b):
-    ...
+def huajian(k):
+    ret = ""
+    k1, k2 = map(int, k.split("/"))
+    
+    # get sign
+    if k1 < 0:
+        sign = "-"
+        k1 *= -1
+    else:
+        sign = "+"
+
+    # get 整数部分
+    zhengshu = k1 // k2
+    k1 %= k2
+    
+    if k1 > 0:
+        if zhengshu > 0:
+            ret += str(zhengshu)
+            ret += " "
+        zdgys = zdgy(k1, k2)
+        k1 //= zdgys
+        k2 //= zdgys
+        ret += str(k1) + "/" + str(k2)
+    elif zhengshu == 0:
+        return "0"
+    else:
+        ret += str(zhengshu)
+        
+
+    if sign == "-":
+        ret = "(-" + ret + ")"
+
+    return ret
 
 
 def plus(a, b):
-    ...
+    c1 = a1 * b2 + b1 * a2
+    c2 = a2 * b2
+    c = str(c1) + "/" + str(c2)
+    return huajian(a) + " + " + huajian(b) + " = " + huajian(c)
+
+
+def minus(a, b):
+    c1 = a1 * b2 - b1 * a2
+    c2 = a2 * b2
+    c = str(c1) + "/" + str(c2)
+    return huajian(a) + " - " + huajian(b) + " = " + huajian(c)
 
 
 def times(a, b):
-    ...
+    c1 = a1 * b1
+    c2 = a2 * b2
+    c = str(c1) + "/" + str(c2)
+    return huajian(a) + " * " + huajian(b) + " = " + huajian(c)
 
 
 def division(a, b):
-    ...
+    ret = ""
+    b2 = ""
+    if b2 == "0":
+        ret = "Inf"
+    else:
+        ...
+    return ret
     
 
 a, b = input().split()
-
+a1, a2 = map(int, a.split("/"))
+b1, b2 = map(int, b.split("/"))
+print(plus(a, b))
+print(minus(a, b))
+print(times(a, b))
+print(division(a, b))
 
 
 # @pintia code=end

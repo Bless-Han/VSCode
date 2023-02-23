a99e74eee14cc5a72b8eca505a78d4954acc9904
Bless-Han <327496836@qq.com>
2023-02-23 08:30:26 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1028 人口普查.py
+++ pintia/PAT (Basic Level) Practice （中文）/1028 人口普查.py
@@ -9,31 +9,43 @@
    
 '''
 # @pintia code=start
-n = int(input())
-old = {"name": "", "birthday": ""}
-young = {"name": "", "birthday": ""}
-count = 0
-for i in range(n):
-    name, birthday_str = input().split()
-    if "1814/09/06" <= birthday_str <= "2014/09/06":
-        count += 1
-        if old["name"] == "":
-            old["name"] = name
-            old["birthday"] = birthday_str
-            young["name"] = name
-            young["birthday"] = birthday_str
-        else:
-            if old["birthday"] > birthday_str:
-                old["name"] = name
-                old["birthday"] = birthday_str
-            if young["birthday"] < birthday_str:
-                young["name"] = name
-                young["birthday"] = birthday_str
+# Import datetime module to compare dates
+from datetime import datetime, timedelta
 
-if count > 0:
-    print(count, old["name"], young["name"])
-elif count == 0:
-    print("0")
+# Get the current date
+current_date = datetime(2014, 9, 6)
+
+# Get the number of people
+n = int(input())
+
+# Initialize variables for oldest and youngest person
+oldest_person = ""
+youngest_person = ""
+oldest_date = current_date - timedelta(days=365*200)
+youngest_date = current_date + timedelta(days=365*200)
+
+# Loop through each person's information
+for i in range(n):
+    name, birthdate = input().split()
+    year, month, day = map(int, birthdate.split('/'))
+    birthdate = datetime(year, month, day)
+    
+    # Check if birthdate is valid
+    if birthdate > current_date or current_date - birthdate > timedelta(days=365*200):
+        continue
+    
+    # Check if this person is the oldest or youngest so far
+    if birthdate < oldest_date:
+        oldest_date = birthdate
+        oldest_person = name
+    if birthdate > youngest_date:
+        youngest_date = birthdate
+        youngest_person = name
+
+# Output the results
+output_list = [oldest_person, youngest_person]
+output_list = [x for x in output_list if x != ""]
+print(len(output_list), *output_list)
 
 
 # @pintia code=end

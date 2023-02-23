a2d7ca44e55f9477e075266f6c5d1d933eca9c5d
Bless-Han <327496836@qq.com>
2023-02-23 16:31:00 +0800

    origin/main

--- pintia/PAT (Basic Level) Practice （中文）/1049 数列的片段和.py
+++ pintia/PAT (Basic Level) Practice （中文）/1049 数列的片段和.py
@@ -10,14 +10,15 @@
 '''
 # @pintia code=start
 import sys
+import decimal
 aaaaaaa = input()
-numbers = list(map(float, sys.stdin.readline().split()))
-result = 0
+numbers = list(map(decimal.Decimal, sys.stdin.readline().split()))
+result = decimal.Decimal(0)
 
 # 临时计算
-temp_a = 0
-# 求一个数的总
-sum_a_number = 0
+temp_a = decimal.Decimal(0)
+# 求一个数的总计
+sum_a_number = decimal.Decimal(0)
 for i in range(len(numbers)):
     temp_a += numbers[i]
     sum_a_number += temp_a
@@ -26,10 +27,11 @@ for i in range(len(numbers)):
     if i == 0:
         result += sum_a_number
         continue
-    # TODO
+    else:
+        # 求当前这个数的总计
+        sum_a_number -= numbers[i - 1] * (len(numbers) - i + 1)
 
-
-    
+        result += sum_a_number
 
 print(f"{result:.02f}")
 
@@ -37,5 +39,5 @@ print(f"{result:.02f}")
 # @pintia code=end
 """ @pintia test=start
 3
-1 2 3
+4 3 2 1
 @pintia test=end """

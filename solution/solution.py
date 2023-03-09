476b389f808d164f4960d318326ede8d6a8afcc1
Bless-Han <327496836@qq.com>
2023-03-09 15:45:26 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,13 +1,26 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+students = [
+        {"name": "Ron", "house": "Gryffindor"},
+        {"name": "Draco", "house": "Slytherin"},
+        {"name": "Hermione", "house": "Gryffindor"},
+        {"name": "Harry", "house": "Gryffindor"},
+        ]
 
-class Solution:
-    def reversePrint(self, head: ListNode) -> list[int]:
-        return reversePrint(head.next) + [head.val] if head else []
+def main():
 
+    for student in sorted(students, key=get_name):
+        print(student["name"], student["house"])
 
+def get_name(student):
+    return student["name"]
+
+main()
 
 print("OK")
+
+
+'''
+Draco is in Slytherin
+Harry is in Gryffindor
+Hermione is in Gryffindor
+Ron is in Gryffindor
+'''

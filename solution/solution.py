314da686ba188f31b20b8b4fc08a501c1c589f26
Bless-Han <327496836@qq.com>
2023-03-10 12:14:24 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -7,9 +7,10 @@ class Node:
 
 
 class Solution:
-    import copy
-    import random
+
     def copyRandomList(self, head: 'Node') -> 'Node':
+        import copy
+        import random
         if not head:
             return None
 
@@ -22,25 +23,27 @@ class Solution:
 
         # 求random_index
         random_index = []
-        for node in nodes:
-            if node.random:
-                random_index = nodes.index(node.random)
+        for i in range(len(nodes)):
+            if nodes[i].random:
+                random_index.append(nodes.index(nodes[i].random))
             else:
-                random_index = -1
+                random_index.append(-1)
 
         # copy nodes to new_nodes
         new_nodes = copy.deepcopy(nodes)
+        # To link
         prev = new_nodes[0]
         for i in range(1, len(new_nodes)):
             prev.next = new_nodes[i]
-            if random_index[i] == -1
-                prev.random = None
-            else:
-                prev.random = new_nodes[random_index[i]]
             prev = prev.next
+
+        # count random_index
+        for i in range(len(new_nodes)):
+            if random_index[i] == -1:
+                new_nodes[i].random = None
+            else:
+                new_nodes[i].random = new_nodes[random_index[i]]
 
         return new_nodes[0]
 
 
-
-    print("OK")

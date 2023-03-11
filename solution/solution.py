fb539f693f99acc084bb724ebff494f66519384f
Bless-Han <327496836@qq.com>
2023-03-11 12:47:23 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,13 +1,4 @@
 class Solution:
-    def findRepeatNumber(self, nums: list[int]) -> int:
-        for i in range(len(nums)):
-            while nums[i] != i:
-                print(nums)
-                if nums[i] == nums[nums[i]]:
-                    return nums[i]
-                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
-        return -1
-
-a, b, c = 1, 2, 3
-a, b, c = c, a, b
-print(a, b, c)
+    def missingNumber(self, nums: list[int]) -> int:
+        n = len(nums)
+        return n * (n + 1) // 2 - sum(nums)

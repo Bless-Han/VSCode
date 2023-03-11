a105885265275e4bc365af445aa6dc0e5da59023
Bless-Han <327496836@qq.com>
2023-03-11 10:35:46 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,37 +1,8 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
-"""
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        if not head:
-            return None
-        # insert Node after every node
-        curr = head
-        while curr:
-            copy = Node(curr.val)
-            copy.next = curr.next
-            curr.next = copy
-            curr = curr.next.next
-        # assign random to new Node
-        curr = head
-        while curr:
-            new = curr.next
-            if curr.random:
-                new.random = curr.random.next
-            curr = curr.next.next
-        # split old and new
-        curr = head
-        copy_head = curr.next
-        while curr:
-            copy = curr.next
-            curr.next = curr.next.next
-            if copy.next:
-                copy.next = copy.next.next
-            curr = curr.next
-        return copy_head
-print("OK")
+    def findRepeatNumber(self, nums: list[int]) -> int:
+        for i in range(len(nums)):
+            while nums[i] != i:
+                if nums[i] == nums[nums[i]]:
+                    return nums[i]
+                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+        return -1

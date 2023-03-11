304381ef0369776f7db6df6afa38d3827d419ca4
Bless-Han <327496836@qq.com>
2023-03-11 11:41:32 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -2,7 +2,12 @@ class Solution:
     def findRepeatNumber(self, nums: list[int]) -> int:
         for i in range(len(nums)):
             while nums[i] != i:
+                print(nums)
                 if nums[i] == nums[nums[i]]:
                     return nums[i]
                 nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
         return -1
+
+a, b, c = 1, 2, 3
+a, b, c = c, a, b
+print(a, b, c)

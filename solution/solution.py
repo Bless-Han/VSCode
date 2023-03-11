f0c801acd45799c31f60c3a1477d445553d88332
Bless-Han <327496836@qq.com>
2023-03-11 09:35:41 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,49 +1,37 @@
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-
-
+"""
 class Solution:
-
     def copyRandomList(self, head: 'Node') -> 'Node':
-        import copy
-        import random
         if not head:
             return None
-
-        # 把链接添加到list里
-        nodes = []
+        # insert Node after every node
         curr = head
         while curr:
-            nodes.append(curr)
+            copy = Node(curr.val)
+            copy.next = curr.next
+            curr.next = copy
+            curr = curr.next.next
+        # assign random to new Node
+        curr = head
+        while curr:
+            new = curr.next
+            if curr.random:
+                new.random = curr.random.next
+            curr = curr.next.next
+        # split old and new
+        curr = head
+        copy_head = curr.next
+        while curr:
+            copy = curr.next
+            curr.next = curr.next.next
+            if copy.next:
+                copy.next = copy.next.next
             curr = curr.next
-
-        # 求random_index
-        random_index = []
-        for i in range(len(nodes)):
-            if nodes[i].random:
-                random_index.append(nodes.index(nodes[i].random))
-            else:
-                random_index.append(-1)
-
-        # copy nodes to new_nodes
-        new_nodes = copy.deepcopy(nodes)
-        # To link
-        prev = new_nodes[0]
-        for i in range(1, len(new_nodes)):
-            prev.next = new_nodes[i]
-            prev = prev.next
-
-        # count random_index
-        for i in range(len(new_nodes)):
-            if random_index[i] == -1:
-                new_nodes[i].random = None
-            else:
-                new_nodes[i].random = new_nodes[random_index[i]]
-
-        return new_nodes[0]
-
-
+        return copy_head
+print("OK")

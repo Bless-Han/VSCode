d673d6487bb4fada1e881a3919f6d234a0884876
Bless-Han <327496836@qq.com>
2023-03-12 11:57:11 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,13 +1,22 @@
 class Solution:
-    def vowelStrings(self, words: list[str], left: int, right: int) -> int:
-        aeiou = "aeiou"
-        count = 0
-        for word in words[left:right + 1]:
-            if word[0] in aeiou and word[-1] in aeiou:
-                count += 1
-             
-        return count
+    def findMinimumTime(self, tasks: list[list[int]]) -> int:
+        intervals = []
+        for task in tasks:
+            intervals.append((task[0], task[2]))  # 添加任务开始时间和持续时间
+            intervals.append((task[1], -task[2]))  # 添加任务结束时间和持续时间的负数
 
+        intervals.sort()  # 将时间区间按照时间顺序排序
 
-words = ["are","amy","u"]
+        ans = 0
+        curr_time = 0
+        for time, duration in intervals:
+            ans += max(0, time - curr_time)  # 如果当前时间小于任务开始时间，需要等待
+            curr_time = time
+            ans += duration  # 执行当前任务所需的时间
+
+        return ans
+
+tasks = [[2,3,1],[4,5,1],[1,5,2]]
+tasks = [[1,3,2],[2,5,3],[5,6,2]]
 s = Solution()
+print(s.findMinimumTime(tasks))

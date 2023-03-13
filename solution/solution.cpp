e490b16ae4d21477e88a1593d0b36ae11f399af0
Bless-Han <327496836@qq.com>
2023-03-13 10:38:01 +0800

    origin/main

--- solution/solution.cpp
+++ solution/solution.cpp
@@ -1,7 +1,36 @@
-#include <stdio.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class CQueue {
+    //两个栈，一个出栈，一个入栈
+    private: Stack<Integer> stack1;
+    private: Stack<Integer> stack2;
+    
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+    
+    public int deleteHead() {
+        if(!stack2.isEmpty()){
+            return stack2.pop();
+        }else{
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            return stack2.isEmpty() ? -1 : stack2.pop();
+        }
+    }
+}
 
 int main(){
-    char s[20];
-    scanf("%s", s);
-    printf("hello %s\n", s);
+    CQueue* obj = new CQueue();
+    obj->appendTail(3);
+    int param_2 = obj->deleteHead();
+    return 0;
 }

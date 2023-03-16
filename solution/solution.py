076da916ae1a65c8d1af79422bb6327c62dbf646
Bless-Han <327496836@qq.com>
2023-03-16 10:16:49 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,5 +1,25 @@
-# Definition for singly-linked list.
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ...
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for c in ransomNote:
+            if c in magazine:
+                magazine = magazine.replace(c, "", 1)
+            else:
+                return False
+        return True
+     # Create Counter objects for the ransomNote and magazine strings
+        note, mag = Counter(ransomNote), Counter(magazine)
+        
+        # Check if the intersection of note and mag Counter objects is equal to note Counter object
+        # If it is, it means that all the letters in ransomNote can be formed using the letters in magazine
+        if note & mag == note: return True
+        return False
 
+a = "aa"
+b = "aab"
+s = Solution()
+print(s.canConstruct(a, b))
+
+
+a = "abcdefg"
+a = a.replace("c","")
+a

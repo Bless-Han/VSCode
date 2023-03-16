e8c7952d04591829b7fdcadd117eadc15fb3f609
Bless-Han <327496836@qq.com>
2023-03-16 09:29:24 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,40 +1,5 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
-"""
+# Definition for singly-linked list.
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        if not head:
-            return None
-        curr = head 
-        while curr:
-            copy = Node(curr.val)
-            copy.next = curr.next
-            curr.next = copy
-            curr = curr.next.next
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ...
 
-        curr = head
-        while curr:
-            copy = curr.next
-            if curr.random:
-                copy.random = curr.random.next
-            curr = curr.next.next
-
-        curr = head
-        copy_head = curr.next
-        while curr:
-            copy = curr.next
-            curr.next = curr.next.next
-            if copy.next:
-                copy.next = copy.next.next
-            curr = curr.next
-        return copy_head
-
-
-
-print("Ok")
-gullible

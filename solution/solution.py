a427135fc1d40b047fffcf235320ad9183d94228
Bless-Han <327496836@qq.com>
2023-03-16 11:16:54 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for c in ransomNote:
-            if c in magazine:
-                magazine = magazine.replace(c, "", 1)
-            else:
-                return False
-        return True
-     # Create Counter objects for the ransomNote and magazine strings
-        note, mag = Counter(ransomNote), Counter(magazine)
+        def count(s):
+            ret = {}
+            for c in s:
+                ret[c] = 1 if c not in ret else ret[c] + 1
+            return ret
         
-        # Check if the intersection of note and mag Counter objects is equal to note Counter object
-        # If it is, it means that all the letters in ransomNote can be formed using the letters in magazine
-        if note & mag == note: return True
-        return False
+        ranCount = count(ransomNote)
+        magCount = count(magazine)
+        if ranCount & magCount == ranCount:
+            return True
+        else:
+            return False
 
 a = "aa"
 b = "aab"

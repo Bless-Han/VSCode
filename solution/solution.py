2ab9d22bbedc9199175b4e985d5b113c4fdeea05
Bless-Han <327496836@qq.com>
2023-03-16 12:16:59 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,24 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        def count(s):
-            ret = {}
-            for c in s:
-                ret[c] = 1 if c not in ret else ret[c] + 1
-            return ret
-        
-        ranCount = count(ransomNote)
-        magCount = count(magazine)
-        if ranCount & magCount == ranCount:
-            return True
-        else:
-            return False
-
-a = "aa"
-b = "aab"
-s = Solution()
-print(s.canConstruct(a, b))
+        magCount = {}
+        for c in magazine:
+            magCount[c] = 1 if c not in magCount else magCount[c] + 1
+        for c in ransomNote:
+            if c not in magCount:
+                return False
+            else:
+                magCount[c] -= 1
+            if magCount[c] < 0:
+                return False
+        return True
 
 
-a = "abcdefg"
-a = a.replace("c","")
-a
+60 * 0.83

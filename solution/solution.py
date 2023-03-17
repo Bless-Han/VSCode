f96d703bf92fbb614b063ebc6197dd8c99307d4a
Bless-Han <327496836@qq.com>
2023-03-17 21:50:25 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,18 +1,37 @@
-import re
+def is_prime(n):
+    for x in range(2, int(n**0.5) + 1):
+        if n % x == 0:
+            return False
+    return True
 
 
-students, questions = map(int, input().split())
+primes = []
+n = int(input())
 
-scores = []
-for _ in range(questions):
-    srch = re.search(r"(\d) \d \d (.+)", input())
-    score = {"score": int(srch.group(1)), "answer": srch.group(2), "wrong": 0}
-    scores.append(score)
+final = []
+for _ in range(n):
+    final.append(input())
 
-print(scores)
-for _ in range(students):
-    # (2 a c) (2 b d) (2 a c) (3 a b e).....
-    answers = input().split()
-    for i in range(len(scores)):
-        srch = re.search(r"\(\d (.+)\)", answers[i])
-        print(srch.group(1))
+k = int(input())
+for_checked = []
+for _ in range(k):
+    search = input()
+    print(search, end=": ")
+
+    try:
+        ind = final.index(search)
+    except ValueError:
+        print("Are you kidding?")
+    else:
+        if search in for_checked:
+            print("Checked")
+            continue
+        else:
+            for_checked.append(search)
+        if ind == 0:
+            print("Mystery Award")
+        elif (ind+1) in primes:
+            print("Minion")
+        else:
+            print("Chocolate")
+

a3511f6bcf12c8b2cea644ba681e4fc8120bd2bd
Bless-Han <327496836@qq.com>
2023-03-17 20:49:16 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,31 +1,18 @@
-'''
-ERROR: aaa is not a legal number
+import re
 
-ERROR: 2.3.4 is not a legal number
-ERROR: 7.123 is not a legal number
-The average of 3 numbers is 1.38
-'''
 
-lksfjldksjl = input()
-numbers_s = input().split()
+students, questions = map(int, input().split())
 
-# lists for numbers
-lists = []
-for number in numbers_s:
-    try:
-        number = float(number)
-    except ValueError:
-        print(f"ERROR: {number} is not a legal number")
-    else:
-        if -1000 <= number <= 1000 and number - round(number, 2) == 0:
-            lists.append(number)
-        else:
-            print(f"ERROR: {number} is not a legal number")
+scores = []
+for _ in range(questions):
+    srch = re.search(r"(\d) \d \d (.+)", input())
+    score = {"score": int(srch.group(1)), "answer": srch.group(2), "wrong": 0}
+    scores.append(score)
 
-if len(lists) == 0:
-    print("The average of 0 numbers is Undefined")
-elif len(lists) == 1:
-    print(f"The average of 1 number is {lists[1]}")
-else:
-    print(f"The average of {len(lists)} numbers is {sum(lists) / len(lists):.2f}")
-
+print(scores)
+for _ in range(students):
+    # (2 a c) (2 b d) (2 a c) (3 a b e).....
+    answers = input().split()
+    for i in range(len(scores)):
+        srch = re.search(r"\(\d (.+)\)", answers[i])
+        print(srch.group(1))

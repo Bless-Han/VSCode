76307bb1813c140458c154d772446cc6338c1e9d
Bless-Han <327496836@qq.com>
2023-03-18 21:19:15 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,7 +1,4 @@
-from math import cos
-from math import sin
+import statistics
 
-r1, p1, r2, p2 = map(float, input().split())
-a1, b1 = r1 * cos(p1), r1 * sin(p1)
-a2, b2 = r2 * cos(p2), r2 * sin(p2)
-
+a = [50, 60]
+print(statistics.mean(a))

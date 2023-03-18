389d4c27d960669d60f21a4b8708c39c8ae588fe
Bless-Han <327496836@qq.com>
2023-03-18 11:37:59 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,56 +1,13 @@
-import sys
 
-primes = {2}
-def is_prime(n):
-    for prime in primes:
-        if n % prime == 0:
-            return False
-    return True
-
-for x in range(2, 10**4+1):
-    if is_prime(x):
-        primes.add(x)
-
-final = []
-for i in range(10**4):
-    final.append(-1)
-
-n = int(input())
-for i in range(1, n + 1):
-    final[int(sys.stdin.readline().strip())] = i
-
-k = int(input())
-for_checked = set()
-for _ in range(k):
-    search = int(sys.stdin.readline().strip())
-    print(f"{search:04}", end=": ")
-
-    try:
-        paiming = final[search]
-    except ValueError:
-        print("Are you kidding?")
-    else:
-        if paiming == -1:
-            print("Are you kidding?")
-            continue
-
-        if search in for_checked:
-            print("Checked")
-            continue
-        else:
-            for_checked.add(search)
-
-        if paiming == 1:
-            print("Mystery Award")
-        elif paiming in primes:
-            print("Minion")
-        else:
-            print("Chocolate")
-
-
-print(7 / 18)
-
-print(5 / 12)
-
-print(7 / 12)
-print(13 / 20)
+result = 1
+wtl = {}
+for _ in range(3):
+    w, t, l = map(float, input().split())
+    wtl["W"] = w
+    wtl["T"] = t
+    wtl["L"] = l
+    # max
+    mx = max(wtl.items(), key=lambda x: x[1])
+    print(mx[0] + " ", end="")
+    result *= mx[1]
+print(f"{(result * 0.65 - 1) * 2:.2f}")

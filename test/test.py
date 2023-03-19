c18ca586c2d0e914a1d9c5162d4e94934cd28914
Bless-Han <327496836@qq.com>
2023-03-19 12:06:31 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,17 +1,46 @@
+grids = [
+        [ 0,11,16, 5,20],
+        [17, 4,19,10,15],
+        [12, 1, 8,21, 6],
+        [ 3,18,23,14, 9],
+        [24,13, 2, 7,22]
+        ]
 class Solution:
-    def evenOddBit(self, n: int) -> list[int]:
-        index = 0
-        even = 0
-        odd = 0
-        while n > 0:
-            if n & 1 == 1:
-                if index % 2 == 0:
-                    even += 1
-                else:
-                    odd += 1
-            n >>= 1
-            index += 1
-        return [even, odd]
+    def checkValidGrid(self, grid: list[list[int]]) -> bool:
+        class Grid:
+            def put_xy(self, x, y):
+                self.x = x
+                self.y = y
+            def judge(self, grid2):
+                a = grid2.x - self.x
+                b = grid2.y - self.y
+                return abs(abs(grid2.x - self.x) - abs(grid2.y - self.y)) == 1
+                return False
+
+
+        tour = []
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                tour.append(Grid())
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                tour[grid[i][j]].put_xy(i, j)
+
+        for t in tour:
+            print(t.x, t.y)
+
+        for i in range(len(tour)-1):
+            if tour[i].judge(tour[i+1]) == False:
+                print("-" * 20)
+                print(tour[i].x, tour[i].y)
+                print(tour[i+1].x, tour[i+1].y)
+                return False
+                
+
+
+
+        return True
 
 s = Solution()
-assert s.evenOddBit(17) == [2,0]
+print(s.checkValidGrid(grids))

785a1d4157df8a1d4bf2ee148a9111526cabea8c
Bless-Han <327496836@qq.com>
2023-03-20 12:06:07 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,4 +1,6 @@
 def is_prime(n):
+    if n == 0 or n == 1:
+        return False
     for x in range(2, int(n**0.5) + 1):
         if n % x == 0:
             return False
@@ -18,19 +20,33 @@ def change_radix(n, d):
     return ret
 
 
-while True:
-    try:
-        n, d = map(int, input().split())
-    except ValueError:
-        break
-    if n < 0:
-        break
-    if is_prime(n) and is_prime(change_radix(n, d)):
-        print("Yes")
-    else:
-        print("No")
+def main():
+    while True:
+        try:
+            n, d = map(int, input().split())
+        except ValueError:
+            break
+        if n < 0:
+            break
+        if is_prime(n) and is_prime(change_radix(n, d)):
+            print("Yes")
+        else:
+            print("No")
+
+def to_base_d(num, d):
+    if num == 0:
+        return "0"
+    result = ""
+    while num > 0:
+        num, rem = divmod(num, d)
+        result = str(rem) + result
+    return result
+
+def from_base_d(num, d):
+    result = 0
+    for digit in num:
+        result = result * d + int(digit)
+    return result
 
 
-number = 73
-bin_str = bin(number)
-bin_str
+print(from_base_d("1001001", 2))

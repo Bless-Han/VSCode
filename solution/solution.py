a0b9b407a3dd5934ea7ba4d42140e2752d9db251
Bless-Han <327496836@qq.com>
2023-03-20 14:07:21 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,52 +1,24 @@
-def is_prime(n):
-    if n == 0 or n == 1:
-        return False
-    for x in range(2, int(n**0.5) + 1):
-        if n % x == 0:
-            return False
-    return True
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
 
-def change_radix(n, d):
-    radix_str = ""
-    while n:
-        radix_str += str(n%d)
-        n //= d
-    ret = 0
-    zhishu = 0
-    while radix_str:
-        ret += int(radix_str[-1]) * (d**zhishu)
-        radix_str = radix_str[:-1]
-        zhishu += 1
-    return ret
+    def __str__(self):
+        return f"{self.name} {self.house}"
 
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin", "SY"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 def main():
-    while True:
-        try:
-            n, d = map(int, input().split())
-        except ValueError:
-            break
-        if n < 0:
-            break
-        if is_prime(n) and is_prime(change_radix(n, d)):
-            print("Yes")
-        else:
-            print("No")
+    s = Student("Neo", "SY")
+    s.house = "Gryffindor"
+    print(s)
 
-def to_base_d(num, d):
-    if num == 0:
-        return "0"
-    result = ""
-    while num > 0:
-        num, rem = divmod(num, d)
-        result = str(rem) + result
-    return result
-
-def from_base_d(num, d):
-    result = 0
-    for digit in num:
-        result = result * d + int(digit)
-    return result
-
-
-print(from_base_d("1001001", 2))
+main()

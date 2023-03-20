04cbda30716637ae97c5f0de1a1161a4119e02ae
Bless-Han <327496836@qq.com>
2023-03-20 08:02:03 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,13 +1,34 @@
+def is_prime(n):
+    for x in range(2, int(n**0.5) + 1):
+        if n % x == 0:
+            return False
+    return True
 
-result = 1
-wtl = {}
-for _ in range(3):
-    w, t, l = map(float, input().split())
-    wtl["W"] = w
-    wtl["T"] = t
-    wtl["L"] = l
-    # max
-    mx = max(wtl.items(), key=lambda x: x[1])
-    print(mx[0] + " ", end="")
-    result *= mx[1]
-print(f"{(result * 0.65 - 1) * 2:.2f}")
+def change_radix(n, d):
+    radix_str = ""
+    while n:
+        radix_str += str(n%d)
+        n //= d
+    ret = 0
+    zhishu = 0
+    print(n, radix_str, ret)
+    while radix_str:
+        ret += int(radix_str[-1]) * (d**zhishu)
+        radix_str = radix_str[:-1]
+        zhishu += 1
+    print(n, radix_str, ret)
+    return ret
+
+
+while True:
+    try:
+        n, d = map(int, input().split())
+    except ValueError:
+        break
+    if n < 0:
+        break
+    if is_prime(n) and is_prime(change_radix(n, d)):
+        print("Yes")
+    else:
+        print("No")
+

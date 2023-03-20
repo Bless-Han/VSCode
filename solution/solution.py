c26f8f15206c9c9c4c286e57ed57633aab1b508a
Bless-Han <327496836@qq.com>
2023-03-20 16:30:48 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -5,4 +5,5 @@ r1, p1, r2, p2 = map(float, input().split())
 a = complex(r1*cos(p1), r1*sin(p1))
 b = complex(r2*cos(p2), r2*sin(p2))
 c = a * b
-print(f"{c.real:.2f}")
+
+print(f"{c.real+0.0001:.2f}{c.imag+0.0001:+.2f}i")

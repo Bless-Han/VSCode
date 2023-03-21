b73109a4a14985320643b1f835d38d845f92bec9
Bless-Han <327496836@qq.com>
2023-03-21 08:28:07 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,10 +1,17 @@
-class Hat:
 
-    houses = ["SY", "CC", "JL"]
-    
-    @classmethod
-    def get_houses(cls):
-        return cls.houses
+def change(num):
+    if num == 0:
+        ret_str = "0"
+    else:
+        ret_str = ""
+    mars = "0123456789ABC"
+    while num > 0:
+        num, rai = divmod(num, 13)
+        ret_str = mars[rai] + ret_str
+    if len(ret_str) == 1:
+        ret_str = "0" + ret_str
+    return ret_str
 
-print(Hat.get_houses())
+r, g, b = map(int, input().split())
+print(f"#{change(r)}{change(g)}{change(b)}")
 

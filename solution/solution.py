977dc49450ac3f66b1fcf687f130bbccd4b043dd
Bless-Han <327496836@qq.com>
2023-03-21 09:28:11 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,17 +1,22 @@
 
-def change(num):
-    if num == 0:
-        ret_str = "0"
-    else:
-        ret_str = ""
-    mars = "0123456789ABC"
-    while num > 0:
-        num, rai = divmod(num, 13)
-        ret_str = mars[rai] + ret_str
-    if len(ret_str) == 1:
-        ret_str = "0" + ret_str
-    return ret_str
+class Cursor:
+    x = 0
+    y = 0
 
-r, g, b = map(int, input().split())
-print(f"#{change(r)}{change(g)}{change(b)}")
+s = input()
+row_count = len(s) // 3
+ver_count = len(s) - row_count * 2
 
+result = [list(" " * ver_count)] * (row_count + 1)
+
+curr = Cursor()
+for c in s:
+    result[curr.x][curr.y] = c
+    print(result[curr.x][curr.y], curr.x, curr.y, c)
+    if curr.x < row_count and curr.y == 0:
+        curr.x += 1
+
+for row in result:
+    print(row)
+for row in result:
+    print("".join(row))

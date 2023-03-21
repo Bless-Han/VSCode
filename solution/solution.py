c8cd61c6354db84c6566872d8c0e04a339a09a26
Bless-Han <327496836@qq.com>
2023-03-21 16:43:22 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,15 +1,1 @@
-def judge(numbers):
-    dic = {}
-    for number in numbers:
-        if number in dic:
-            dic[number] += 1
-        else:
-            dic[number] = 1
-
-    for d in dic:
-        if dic[d] == 1:
-            return str(d)
-    return "None"
-
-numbers = list(map(int, input().split()))[1:]
-print(judge(numbers))
+20 + "klk"
--- test/test.py
+++ test/test.py
@@ -1,55 +1,5 @@
-grids = [
-        [ 0,11,16, 5,20],
-        [17, 4,19,10,15],
-        [12, 1, 8,21, 6],
-        [ 3,18,23,14, 9],
-        [24,13, 2, 7,22]
-        ]
-class Solution:
-    def checkValidGrid(self, grid: list[list[int]]) -> bool:
-        if grid[0][0] != 0:
-            return False
-        class Grid:
-            def put_xy(self, x, y):
-                self.x = x
-                self.y = y
-            def judge(self, grid2):
-                a = grid2.x - self.x
-                b = grid2.y - self.y
-                if a > 0 and b > 0:
-                    return (a == 2 and b == 1) or (a == 1 and b == 2)
-                elif a < 0 and b < 0:
-                    return (a == -2 and b == -1) or (a == -1 and b == -2)
-                elif a < 0 and b > 0:
-                    return (a == -2 and b == 1) or (a == -1 and b == 2)
-                else:
-                    return (a == 2 and b == -1) or (a == 1 and b == -2)
+from datetime import date
 
-                return False
+toda = date.today()
 
-        tour = []
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                tour.append(Grid())
-
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                tour[grid[i][j]].put_xy(i, j)
-
-        for t in tour:
-            print(t.x, t.y)
-
-        for i in range(len(tour)-1):
-            if tour[i].judge(tour[i+1]) == False:
-                print("-" * 20)
-                print(tour[i].x, tour[i].y)
-                print(tour[i+1].x, tour[i+1].y)
-                return False
-                
-
-
-
-        return True
-
-s = Solution()
-print(s.checkValidGrid(grids))
+print(toda)

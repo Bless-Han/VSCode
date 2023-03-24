4d9659b5aa5aeeda078a28b469034e8816903c9b
Bless-Han <327496836@qq.com>
2023-03-24 10:11:45 +0800

    origin/main

---
+++ pintia/PAT (Advanced Level) Practice/1042 Shuffling Machine.py
@@ -0,0 +1,14 @@
+'''
+   @pintia psid=994805342720868352 pid=994805442671132672 compiler=PYTHON3
+   
+   ProblemSet: PAT (Advanced Level) Practice
+   
+   Title: 1042 Shuffling Machine
+   
+   https://pintia.cn/problem-sets/994805342720868352/exam/problems/994805442671132672
+   
+'''
+# @pintia code=start
+
+
+# @pintia code=end

e45123b496a1f3537844be3d481b23f55de2c619
Bless-Han <327496836@qq.com>
2023-03-25 15:33:44 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,3 +1,21 @@
-a = [1, 2, 3, 4]
-list(a[::-1])
+balance = 0
 
+
+def main():
+    print("balance:", balance)
+    deposit(100)
+    withdraw(50)
+    print("balance:", balance)
+
+def deposit(money):
+    global balance
+    balance += money
+
+
+def withdraw(money):
+    global balance
+    balance -= money
+
+
+if __name__ == "__main__":
+    main()

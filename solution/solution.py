1ba96c7eb37cf9bacd61fea25cd788ed3a332f0b
Bless-Han <327496836@qq.com>
2023-03-27 17:05:33 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,10 +1,4 @@
-a = input().split()
-b = input().split()
-c = []
-for n in a:
-    if n in b:
-        c.append(n)
-if len(c) == 0:
-    print("NULL")
-else:
-    print(*c)
+import sys
+
+for line in sys.stdin:
+    print(line)

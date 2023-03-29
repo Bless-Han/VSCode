22afeb9520152d62ea0fc77acc8f4aae77930b5a
Bless-Han <327496836@qq.com>
2023-03-29 18:10:21 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,3 +1,9 @@
-a = "abcdcce"
-a.replace("c", "")
-print(a)
+# Python tkinter hello world program
+  
+from tkinter import *
+  
+root = Tk()
+a = Label(root, text ="Hello World")
+a.pack()
+  
+root.mainloop()

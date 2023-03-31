ef208116d816f5518c5512e074721b1e9cedc496
Bless-Han <327496836@qq.com>
2023-03-31 08:43:15 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -15,7 +15,6 @@ mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-
 feet = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))

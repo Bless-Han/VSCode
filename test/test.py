66b449a0b450c570b89c949ec7e45d45eab63eef
Bless-Han <327496836@qq.com>
2023-04-01 21:45:49 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,4 +1,9 @@
 a = input()
-n = 0
-print("{:.{}f}".format(float(a), 0))
-
+e_l, exp = a.split("E")
+p_l, p_r = e_l.split(".")
+n = -(len(p_r)) + int(exp)
+if n > 0:
+    n = 0
+else:
+    n = -n
+print(f"{float(a):.{n}f}")

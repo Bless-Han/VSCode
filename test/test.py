abea9e4cff7dc87248c1c83fb282848ed965572b
Bless-Han <327496836@qq.com>
2023-04-01 20:32:25 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,20 +1,4 @@
-from copy import deepcopy
-class Dict(dict):
-    def __init__(self, flag=0):
-        if flag == 1:
-            numbers = list(map(int, input().split()))[1:]
-            for i in range(0, len(numbers), 2):
-                dict[i+1] = i
+a = input()
+n = 0
+print("{:.{}f}".format(float(a), 0))
 
-    def __str__(self):
-        ret = []
-        self.clear()
-        if not self:
-            return "0 0"
-        for k, v in sorted(self.items(), reverse=True):
-            ret.append()
-        return " ".join(str(n) for n in ret)
-
-a = []
-a.append("a")
-a.append("b")

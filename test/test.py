78741fcacec544fce2b50dd5abe739bc2a98a65d
Bless-Han <327496836@qq.com>
2023-04-01 18:32:16 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,45 +1,23 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-        self.parent = None
+class Dict(dict):
+    def __init__(self):
+        s = list(map(int, input().split()))[1:]
+        for i in range(0, len(s), 2):
+            if s[i] in self:
+                self[s[i+1]] += s[i]
+            else:
+                self[s[i+1]] = s[i]
 
-def isomorphic(root1, root2):
-    if root1 is None and root2 is None:
-        return True
-    if root1 is None or root2 is None:
-        return False
-    if root1.data != root2.data:
-        return False
-    return (isomorphic(root1.left, root2.left) and isomorphic(root1.right, root2.right))\
-            or (isomorphic(root1.left, root2.right) and isomorphic(root1.right, root2.left))
+    def __str__(self):
+        return "Hello world"
 
-def build_tree():
-    n = int(input())
-    nodes = []
-    for _ in range(n):
-        nodes.append(Node(0))
-    for i in range(n):
-        data, left, right = input().split()
-        nodes[i].data = data
-        if left != '-':
-            nodes[i].left = nodes[int(left)]
-            nodes[int(left)].parent = nodes[i]
-        if right != '-':
-            nodes[i].right = nodes[int(right)]
-            nodes[int(right)].parent = nodes[i]
-    return get_root(nodes)
+    def __add__(self, other):
+        ...
 
-def get_root(nodes):
-    for node in nodes:
-        if node.parent == None:
-            return node
+    def __mul__(self, other):
+        ...
 
-if __name__ == '__main__':
-    root1 = build_tree()
-    root2 = build_tree()
-    if isomorphic(root1, root2):
-        print('Yes')
-    else:
-        print('No')
+a = Dict()
+b = Dict()
+print(a + b)
+print(a * b)
+

3a014dbcc6d66eabcae859ea0e584909ccd16585
Bless-Han <327496836@qq.com>
2023-04-02 10:01:53 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,9 +1,7 @@
+import decimal
 a = input()
 e_l, exp = a.split("E")
 p_l, p_r = e_l.split(".")
 n = -(len(p_r)) + int(exp)
-if n > 0:
-    n = 0
-else:
-    n = -n
-print(f"{float(a):.{n}f}")
+n = -n if n < 0 else 0
+print(f"{decimal.Decimal(a):.{n}f}")

e5117a6217b107c1cc9880ab80165f637fe3121b
Bless-Han <327496836@qq.com>
2023-04-03 17:29:48 +0800

    origin/main

--- test/project.py
+++ test/project.py
@@ -1,44 +1,69 @@
 from tkinter import *
 from tkinter import ttk
 
+def main():
+    root = Tk()
+    root.title("Base Converter")
+
+    mainframe = ttk.Frame(root, padding="12 3 12 3")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    ttk.Label(mainframe, text="From:", width=10).grid(column=1, row=1, sticky=(W, N))
+    ttk.Label(mainframe, text="To:", width=10).grid(column=2, row=1, sticky=W)
+
+    before = StringVar()
+    before_entry = ttk.Entry(mainframe, width=10, textvariable=before)
+    before_entry.grid(column=1, row=2, sticky=W)
+    before_entry.bind("<Key>", on_entry_change)
+
+    after_label = ttk.Label(mainframe, text="", width=10)
+    after_label.grid(column=2, row=2, sticky=W)
+    after_label.config(text="neoneo")
+
+    listbox_left = Listbox(mainframe, width=12, height=4, bg="systemTransparent")
+    listbox_left.grid(column=1, row=3, sticky=W)
+    listbox_left.insert(0, "Binary")
+    listbox_left.insert(1, "Octal")
+    listbox_left.insert(2, "Decimal")
+    listbox_left.insert(3, "Hex")
+    listbox_left.selection_set(0)
+
+    listbox_right = Listbox(mainframe, width=12, height=4, bg="systemTransparent")
+    listbox_right.grid(column=2, row=3, sticky=W)
+    listbox_right.insert(0, "Binary")
+    listbox_right.insert(1, "Octal")
+    listbox_right.insert(2, "Decimal")
+    listbox_right.insert(3, "Hex")
+
+    for child in mainframe.winfo_children():
+        child.grid_configure(padx=5, pady=5)
+
+    before_entry.focus()
+    root.bind("<Return>", calculate)
+
+    root.mainloop()
+
 def calculate(*args):
     try:
         value = float(feet.get())
         meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
-root = Tk()
-root.title("Base Converter")
 
-mainframe = ttk.Frame(root, padding="12 3 12 3")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+def on_entry_change(event):
+    print(event.widget.get())
+    # TODO: 监测事件，并修改其他控件内容
+    after_label.config(text="neoneo")
 
-before = StringVar()
-before_entry = ttk.Entry(mainframe, width=10, textvariable=before)
-before_entry.grid(column=1, row=2, sticky=W)
+def on_select_left(event):
+    # TODO: 监测事件，并修改其他控件内容
+    ...
 
-after = StringVar()
-after_entry = ttk.Entry(mainframe, width=10, textvariable=after)
-after_entry.grid(column=2, row=2, sticky=(W, E))
+def on_select_right(event):
+    # TODO: 监测事件，并修改其他控件内容
+    ...
 
-ttk.Label(mainframe, text="From:", width=10).grid(column=1, row=1, sticky=(W, N))
-ttk.Label(mainframe, text="To:", width=10).grid(column=2, row=1, sticky=W)
-
-listbox = Listbox(mainframe, width=10, height=4, bg="systemTransparent")
-listbox.grid(column=1, row=3, sticky=W)
-listbox.insert(1, "Binary")
-listbox.insert(2, "Octal")
-listbox.insert(3, "Decimal")
-listbox.insert(4, "Hex")
-listbox.selection_set(4)
-
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-
-before_entry.focus()
-root.bind("<Return>", calculate)
-
-root.mainloop()
-
+if __name__ == "__main__":
+    main()
--- test/test.py
+++ test/test.py
@@ -1,18 +1,22 @@
 import tkinter as tk
 
+def on_select(event):
+    # 获取当前选中的项
+    selected_item = my_listbox.get(my_listbox.curselection())
+    # 在控制台中打印选中的项
+    print(selected_item)
+
 root = tk.Tk()
 
-# 创建一个Listbox
-listbox = tk.Listbox(root)
+# 创建一个Listbox并添加选项
+my_listbox = tk.Listbox(root)
+my_listbox.insert(0, "Option 1")
+my_listbox.insert(1, "Option 2")
+my_listbox.insert(2, "Option 3")
+my_listbox.pack()
 
-# 向Listbox中添加项目
-listbox.insert(1, "项目1")
-listbox.insert(2, "项目2")
-listbox.insert(3, "项目3")
-listbox.insert(4, "项目4")
-
-# 将Listbox放置在窗口中
-listbox.pack()
+# 绑定<<ListboxSelect>>事件处理程序
+my_listbox.bind("<<ListboxSelect>>", on_select)
 
 root.mainloop()
 

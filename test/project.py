f740421cc73dbe0f1b5b95ecc1f647cce8560f7b
Bless-Han <327496836@qq.com>
2023-04-04 17:36:49 +0800

    origin/main

--- test/project.py
+++ test/project.py
@@ -1,11 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 
+root = Tk()
+mainframe = ttk.Frame(root, padding="12 3 12 3")
+before = StringVar()
+before_entry = ttk.Entry(mainframe, width=10, textvariable=before)
+after_label = ttk.Label(mainframe, text="", width=10)
+listbox_left = Listbox(mainframe, width=12, height=4, bg="systemTransparent")
+listbox_right = Listbox(mainframe, width=12, height=4, bg="systemTransparent")
+
 def main():
-    root = Tk()
     root.title("Base Converter")
 
-    mainframe = ttk.Frame(root, padding="12 3 12 3")
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
@@ -13,16 +19,10 @@ def main():
     ttk.Label(mainframe, text="From:", width=10).grid(column=1, row=1, sticky=(W, N))
     ttk.Label(mainframe, text="To:", width=10).grid(column=2, row=1, sticky=W)
 
-    before = StringVar()
-    before_entry = ttk.Entry(mainframe, width=10, textvariable=before)
     before_entry.grid(column=1, row=2, sticky=W)
-    before_entry.bind("<Key>", on_entry_change)
 
-    after_label = ttk.Label(mainframe, text="", width=10)
     after_label.grid(column=2, row=2, sticky=W)
-    after_label.config(text="1")
 
-    listbox_left = Listbox(mainframe, width=12, height=4, bg="systemTransparent")
     listbox_left.grid(column=1, row=3, sticky=W)
     listbox_left.insert(0, "Binary")
     listbox_left.insert(1, "Octal")
@@ -30,7 +30,6 @@ def main():
     listbox_left.insert(3, "Hex")
     listbox_left.selection_set(0)
 
-    listbox_right = Listbox(mainframe, width=12, height=4, bg="systemTransparent")
     listbox_right.grid(column=2, row=3, sticky=W)
     listbox_right.insert(0, "Binary")
     listbox_right.insert(1, "Octal")
@@ -39,31 +38,40 @@ def main():
 
     for child in mainframe.winfo_children():
         child.grid_configure(padx=5, pady=5)
+        
+    listening_event()
 
     before_entry.focus()
-    root.bind("<Return>", calculate)
-
     root.mainloop()
+    
+def listening_event():
+    before_entry.bind("<Key>", on_entry_change)
+    listbox_left.bind("<<ListboxSelect>>", on_select_left)
+    listbox_right.bind("<<ListboxSelect>>", on_select_right)
+
 
 def calculate(*args):
     try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+        value = int(before_entry.get())
     except ValueError:
-        pass
+        after_label.config(text="Invalid value.")
+    else:
+        after_label.config(text=value)
 
 def on_entry_change(event):
+    # test
     print(event.widget.get())
     # TODO: 监测事件，并修改其他控件内容
-
+    after_label.config(text=before_entry.get())
+    # calculate()
 
 def on_select_left(event):
     # TODO: 监测事件，并修改其他控件内容
-    ...
+    after_label.config(text="left_test")
 
 def on_select_right(event):
     # TODO: 监测事件，并修改其他控件内容
-    ...
+    after_label.config(text="right_test")
 
 if __name__ == "__main__":
     main()
--- test/temp.py
+++ test/temp.py
@@ -1,42 +1,16 @@
-'''
-堆中的路径
-分数 25   提交人数 13668   通过人数 4854   通过率 35.51%
-作者 陈越     单位 浙江大学
-将一系列给定数字依次插入一个初始为空的小顶堆H[]。随后对任意给定的下标i，打印从H[i]到根结点的路径。
+import tkinter as tk
 
-输入格式:
-每组测试第1行包含2个正整数NN和MM(\le 1000≤1000)，分别是插入元素的个数、以及需要打印的路径条数。下一行给出区间[-10000, 10000]内的NN个要被插入一个初始为空的小顶堆的整数。最后一行给出MM个下标。
+class SampleApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.lb = tk.Listbox(self)
+        for x in range(20):
+            self.lb.insert("end", x)
+        self.lb.bind("<<ListboxSelect>>", self.OnSelect)
+        self.lb.pack(side="top", fill="both", expand=True)
 
-输出格式:
-对输入中给出的每个下标i，在一行中输出从H[i]到根结点的路径上的数据。数字间以1个空格分隔，行末不得有多余空格。
+    def OnSelect(self, event):
+        print(event.widget.get(event.widget.curselection()))
 
-输入样例:
-5 3
-46 23 26 24 10
-5 4 3
-输出样例:
-24 23 10
-46 23 10
-26 10
-'''
-
-N, M = map(int, input().split())
-H = [0]
-nums = list(map(int, input().split()))
-for num in nums:
-    H.append(num)
-    i = len(H) - 1
-    while i > 1:
-        if H[i] < H[i // 2]:
-            H[i], H[i // 2] = H[i // 2], H[i]
-            i = i // 2
-        else:
-            break
-for i in map(int, input().split()):
-    path = []
-    while i > 0:
-        path.append(H[i])
-        i = i // 2
-    print(*path)
-
-# Path: test/temp.py
+app = SampleApp()
+app.mainloop()

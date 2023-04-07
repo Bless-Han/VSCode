96d1af3c442a977dd42cd16a7abfda7eb2e14dca
Bless-Han <327496836@qq.com>
2023-04-07 17:58:43 +0800

    origin/main

--- test/project.py
+++ test/project.py
@@ -3,15 +3,13 @@ from tkinter import ttk
 
 root = Tk()
 mainframe = ttk.Frame(root, padding="12 3 12 3")
+label = ttk.Label(mainframe, text="Enter a number.", width=20, foreground="green")
 before = StringVar()
-before_entry = ttk.Entry(mainframe, width=20, textvariable=before)
-after_label = ttk.Label(mainframe, text="", width=20)
-left_listbox = Listbox(mainframe, width=22, height=4, bg="systemTransparent")
-right_text = Text(root, height=4,width=20)
+entry = ttk.Entry(mainframe, width=20, textvariable=before)
+listbox = Listbox(mainframe, width=22, height=4, bg="systemTransparent")
 
 numbers = {}
-left_curselection = (0, )
-right_curselection = (0, )
+curselection = (0, )
 
 def main():
     root.title("Base Converter")
@@ -19,45 +17,58 @@ def main():
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
-
-    ttk.Label(mainframe, text="From:", width=20).grid(column=1, row=1, sticky=(W, N))
-    ttk.Label(mainframe, text="To:", width=20).grid(column=2, row=1, sticky=W)
-
-    before_entry.bind("<KeyRelease>", entry_on_change)
-    before_entry.grid(column=1, row=2, sticky=W)
-
-    after_label.grid(column=2, row=2, sticky=W)
-
-    left_listbox.grid(column=1, row=3, sticky=W)
-    left_listbox.insert(0, "Binary")
-    left_listbox.insert(1, "Octal")
-    left_listbox.insert(2, "Decimal")
-    left_listbox.insert(3, "Hex")
-    left_listbox.selection_set(0)
-    left_listbox.bind("<<ListboxSelect>>", on_select_left)
-
-    right_text.grid(column=2, row=3, sticky=W)
+    label.grid(column=1, row=1, sticky=(W, N))
+    entry.grid(column=1, row=2, sticky=W)
+    entry.bind("<KeyRelease>", entry_on_change)
+    listbox.grid(column=1, row=3, sticky=W)
+    listbox.selection_set(0)
+    listbox.bind("<<ListboxSelect>>", on_select)
+    init_listbox()
 
     for child in mainframe.winfo_children():
         child.grid_configure(padx=5, pady=5)
         
-    before_entry.focus()
-    root.bind("<Return>", convert)
+    entry.focus()
     root.mainloop()
     
 def entry_on_change(event):
     convert()
+    
+def init_listbox():
+    listbox.delete(0)
+    listbox.insert(0, "Binary")
+    listbox.delete(1)
+    listbox.insert(1, "Octal")
+    listbox.delete(2)
+    listbox.insert(2, "Decimal")
+    listbox.delete(3)
+    listbox.insert(3, "Hex")
 
 def convert(*args):
     s = ["Bin", "Oct", "Dec", "Hex"]
-    value = before_entry.get()
-    if check(value, s[left_curselection[0]]):
-        numbers = count(value, s[left_curselection[0]])
-        right_label.config(text=f"{numbers['Bin']}\n{numbers['Oct']}\n{numbers['Dec']}\n{numbers['Hex']}")
-        after_label.config(text="")
+    value = entry.get()
+    if value == "":
+        init_listbox()
+        label.config(text="Enter a number.", foreground="green")
+        return
     else:
-        after_label.config(text="Invalid value.")
+        label.config(text="OK", foreground="green")
+    if check(value, s[curselection[0]]):
+        numbers = count(value, s[curselection[0]])
+        listbox.delete(0)
+        listbox.insert(0, f"Binary {numbers['Bin']}")
+        listbox.delete(1)
+        listbox.insert(1, f"Octal {numbers['Oct']}")
+        listbox.delete(2)
+        listbox.insert(2, f"Decimal {numbers['Dec']}")
+        listbox.delete(3)
+        listbox.insert(3, f"Hex {numbers['Hex']}")
+        print(value)
+    else:
+        init_listbox()
+        label.config(text="Invalid number.", foreground="red")
         
+# Check if the value valid.
 def check(value, base):
     match base:
         case "Bin": return all(c in "01" for c in value)
@@ -65,6 +76,7 @@ def check(value, base):
         case "Dec": return all(c in "0123456789" for c in value)
         case "Hex": return all(c in "0123456789ABCDEF" for c in value.upper())
         
+# Change the base of value to all the others
 def count(value, base):
     decimal = 0
     match base:
@@ -80,10 +92,11 @@ def count(value, base):
         "Hex": hex(decimal)[2:].upper()
     }
 
-def on_select_left(event):
-    global left_curselection
+def on_select(event):
+    global curselection
     if event.widget.curselection():
-        left_curselection = event.widget.curselection()
+        # TODO: Fix this. Change the value of entry.
+        curselection = event.widget.curselection()
     convert()
 
 if __name__ == "__main__":

bc13a9b8e09bcbd5b0527d858a3f90724de38f46
Bless-Han <327496836@qq.com>
2023-04-07 20:15:07 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,11 +1,32 @@
-import tkinter as tk
 
-root = tk.Tk()
 
-t = tk.Text(root)
-t.pack()
 
-t.insert(tk.END, "只读\n模式", width=55)
-t.configure(state='disabled')
+class Dict(dict):
+    def __mul__(self, o):
+        ret = Dict()
+        for k1, v1 in self.items():
+            for k2, v2 in o.items():
+                if v1 + v1 not in ret:
+                    ret[v1+v2] = k1 * k2
+                else:
+                    ret[v1+v2] += k1 * k2
+        return ret
 
-root.mainloop()
+    def __add__(self, o):
+        ret = deepcopy(self)
+        for k, v in o.items():
+            if v not in ret:
+                ret[k] = v
+            else:
+                ret[k] += v
+        return ret
+
+    def __str__(self):
+        return "good"
+
+d1 = Dict()
+d2 = Dict()
+print(d1 * d2)
+print(d1 + d2)
+
+print("OK")

7da9a56f33ba04f765b85cb1d8e158bc48238562
Bless-Han <327496836@qq.com>
2023-04-09 15:24:04 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,11 +1,32 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        return list(set(range(1, len(nums)+1)) - set(nums))
+class MyLinkedList:
 
+    def __init__(self):
+        self = None
+
+    def get(self, index: int) -> int:
+        curr_index = 0
+        curr_node = self
+        return -1
+
+    def addAtHead(self, val: int) -> None:
+        
+
+    def addAtTail(self, val: int) -> None:
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
 
 print()
-
-set1 = {1, 2, 3}
-set2 = [3, 4, 5]
-set2.update(set1)
-print(set2)

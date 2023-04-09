5eeff752c7ea9b498bae756665460280e883c10e
Bless-Han <327496836@qq.com>
2023-04-09 20:40:45 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,24 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+import statistics
 
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        if not headA and not headB:
-            return None
-        a = headA
-        b = headB
-        while a != b:
-            if not a:
-                a = headB
-            else:
-                a = a.next
-            if not b:
-                b = headA
-            else:
-                b = b.next
-        return a
-
-print()
+a = [50, 60]
+print(statistics.mean(a))

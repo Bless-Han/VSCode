d59da9ce6dab289792b2204033a9a4776a4703a2
Bless-Han <327496836@qq.com>
2023-04-09 08:55:16 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -1,22 +1,9 @@
-from tkinter import *
-
-root = Tk()
-
-def testVal(inStr, acttyp):
-    if acttyp == '1': #insert
-        if not inStr.isdigit():
-            return False
-    return True
-
-entry = Entry(root, validate="key")
-# Q: What's this code mean and how does it work?
-# A: It's a callback function that is called when the user types a character into the entry.
-# Q: What is the first argument?
-# A: It's the value of the entry.
-# Q: What is the second argument?
-# A: It's the action type. 1 means insert, 0 means delete, -1 means focus in, -2 means focus out.
-
-entry['validatecommand'] = (entry.register(testVal), '%P', '%d')
-entry.pack()
-
-root.mainloop()
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        max = -1
+        for i in range(len(arr)-1, -1, -1):
+            temp = arr[i]
+            arr[i] = max
+            if temp > max:
+                max = temp
+        return arr

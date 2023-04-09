b675752fa912b1aae4f04337253f5a9a1045cbfc
Bless-Han <327496836@qq.com>
2023-04-09 12:58:09 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -1,9 +1,9 @@
-class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-        if len(nums) < 3:
-            return max(nums)
-        else:
-            nums.remove(max(nums))
-            nums.remove(max(nums))
-            return max(nums)
+a = set()
+b = set()
+a.add(1)
+a.add(3)
+a.add(9)
+b.add(3)
+b.add(20)
+
+# TODO: add a and b
--- test/test.py
+++ test/test.py
@@ -1,21 +1,11 @@
 class Solution:
-    def minimizeMax(self, nums: list[int], p: int) -> int:
-        ret = []
-        l = len(nums)
-        used = set()
-        for i in range(l):
-            min_j = i+1
-            mn = abs(nums[i] - nums[j])
-            for j in range(i+1, l):
-                ...
-            if i not in used and j not in used:
-                used.add(i)
-                used.add(j)
-                ret.append(abs(nums[i]-nums[j]))
-        if not p:
-            return 0
-        else:
-            return max(sorted(ret)[:p])
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        return list(set(range(1, len(nums)+1)) - set(nums))
 
 
 print()
+
+set1 = {1, 2, 3}
+set2 = [3, 4, 5]
+set2.update(set1)
+print(set2)

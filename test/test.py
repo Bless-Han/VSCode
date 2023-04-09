9eb5305893c6f869a42efd465989daa40cceffb9
Bless-Han <327496836@qq.com>
2023-04-09 11:58:05 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,6 +1,21 @@
 class Solution:
-    def distance(self, nums: list[int]) -> list[int]:
-        same_dic = {}
+    def minimizeMax(self, nums: list[int], p: int) -> int:
+        ret = []
+        l = len(nums)
+        used = set()
+        for i in range(l):
+            min_j = i+1
+            mn = abs(nums[i] - nums[j])
+            for j in range(i+1, l):
+                ...
+            if i not in used and j not in used:
+                used.add(i)
+                used.add(j)
+                ret.append(abs(nums[i]-nums[j]))
+        if not p:
+            return 0
+        else:
+            return max(sorted(ret)[:p])
 
 
 print()

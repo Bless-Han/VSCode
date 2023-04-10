9eb6762748f2675f28b94a7c175bb2032415920d
Bless-Han <327496836@qq.com>
2023-04-10 13:21:22 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,21 +1,4 @@
-N, M = map(int, input().split())
-H = [0]
-nums = list(map(int, input().split()))
-for num in nums:
-    H.append(num)
-    i = len(H) - 1
-    while i > 1:
-        if H[i] < H[i // 2]:
-            H[i], H[i // 2] = H[i // 2], H[i]
-            i = i // 2
-        else:
-            break
-print(H)
-for i in map(int, input().split()):
-    path = []
-    while i > 0:
-        path.append(H[i])
-        i = i // 2
-    print(*path)
-
-
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        nums_set = set(nums)
+        return list(set(range(1, len(nums)+1)) - set(nums))

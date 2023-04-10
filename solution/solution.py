f7202620c9a2dd35eeae514c88d969c1d6da5ba0
Bless-Han <327496836@qq.com>
2023-04-10 18:36:59 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,4 +1,6 @@
+course = []
 def main():
+    global course
     n, e = map(int, input().split())
     graph = [[] for _ in range(n)]
     for _ in range(e):
@@ -10,27 +12,41 @@ def main():
     visited = [False] * n
     for i in range(n):
         if not visited[i]:
-            print("{ ", end="")
+            course = []
             dfs(graph, visited, i)
-            print("}")
+            print("{", *course, "}")
     visited = [False] * n
     for i in range(n):
         if not visited[i]:
-            print("{ ", end="")
+            course = []
             bfs(graph, visited, i)
-            print("}")
+            print("{", *course, "}")
 def dfs(graph, visited, u):
-    visited[u] = True
-    print(u, end=' ')
-    for v in graph[u]:
-        if not visited[v]:
-            dfs(graph, visited, v)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def bfs(graph, visited, u):
+    global course
     queue = [u]
     visited[u] = True
     while queue:
         u = queue.pop(0)
-        print(u, end=' ')
+        course.append(u)
         for v in graph[u]:
             if not visited[v]:
                 queue.append(v)

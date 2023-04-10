90e10d9f810ad1757d13cefcfaa8077c7bdef558
Bless-Han <327496836@qq.com>
2023-04-10 16:21:35 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,9 +1,4 @@
-from pytest import raises
+a = {0: 99, 1: 88, 2: 123}
 
-def get_number(s):
-    return int(s)
-
-
-def test_get_number():
-    with raises(ValueError):
-        get_number("9")
+b = list(a.items())
+print("a".format())

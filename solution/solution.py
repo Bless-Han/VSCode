d5a32efdb6ccc04e5b4f03e52c3b6cf1491adf10
Bless-Han <327496836@qq.com>
2023-04-11 18:37:19 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,55 +1,8 @@
-course = []
-def main():
-    global course
-    n, e = map(int, input().split())
-    graph = [[] for _ in range(n)]
-    for _ in range(e):
-        u, v = map(int, input().split())
-        graph[u].append(v)
-        graph[v].append(u)
-    for i in range(n):
-        graph[i].sort()
-    visited = [False] * n
-    for i in range(n):
-        if not visited[i]:
-            course = []
-            dfs(graph, visited, i)
-            print("{", *course, "}")
-    visited = [False] * n
-    for i in range(n):
-        if not visited[i]:
-            course = []
-            bfs(graph, visited, i)
-            print("{", *course, "}")
-def dfs(graph, visited, u):
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def bfs(graph, visited, u):
-    global course
-    queue = [u]
-    visited[u] = True
-    while queue:
-        u = queue.pop(0)
-        course.append(u)
-        for v in graph[u]:
-            if not visited[v]:
-                queue.append(v)
-                visited[v] = True
-if __name__ == '__main__':
-    main()
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        

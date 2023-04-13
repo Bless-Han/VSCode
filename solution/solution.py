ce9e1089250a21927ba1dce967054c4663feef79
Bless-Han <327496836@qq.com>
2023-04-13 14:18:24 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,57 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(l1.val + l2.val)
-        l1 = l1.next
-        l2 = l2.next
-        curr = head
-        while l1 and l2:
-            curr.next = ListNode(l1.val + l2.val)
-            curr = curr.next
-            l1 = l1.next
-            l2 = l2.next
-        while l1:
-            curr.next = ListNode(l1.val)
-            curr = curr.next
-            l1 = l1.next
-        while l2:
-            curr.next = ListNode(l2.val)
-            curr = curr.next
-            l2 = l2.next
-        curr.next = None
+def check(s):
+    try:
+        left_A, right = s.split("P")
+        middle_A, right_A = right.split("T")
+    except Exception:
+        return False
+    else:
+        if not full_A(left_A) or not full_A(middle_A) or not full_A(right_A):
+            return False
+        if len(middle_A) == 0:
+            return False
+        return len(left_A) * len(middle_A) == len(right_A)
 
-        curr = head
-        jinwei = 0
-        if curr.val >= 10:
-            jinwei = curr.val // 10
-            curr.val %= 10
-            curr = curr.next
-        while curr:
-            if jinwei:
-                curr.val += jinwei
-                jinwei = 0
-            if curr.val >= 10:
-                jinwei = curr.val // 10
-                curr.val %= 10
-            curr = curr.next
-        curr = head
-        while curr.next:
-            curr = curr.next
-        if jinwei:
-            curr.next = ListNode(jinwei)
-            jinwei = 0
+def full_A(s):
+    for c in s:
+        if c != "A":
+            return False
+    return True
 
-        # reverse
-        # curr = head
-        # head = None
-        # while curr:
-            # temp = curr.next
-            # curr.next = head
-            # head = curr
-            # curr = temp
-
-        return head
+n = int(input())
+for _ in range(n):
+    print("YES") if check(input()) else print("NO")

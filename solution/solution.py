3eda49181eeb9c3a7d50d034ba57f3fe3f670ae4
Bless-Han <327496836@qq.com>
2023-04-14 17:11:28 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,19 +1,30 @@
+def main():
+    n, m = map(int, input().split())
+    graph = [[0 for _ in range(n)] for _ in range(n)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a-1][b-1] = c
+        graph[b-1][a-1] = c
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if graph[i][j] < graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
+    max_ = 0
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] > max_:
+                max_ = graph[i][j]
+    if max_ == 0:
+        print(0)
+    else:
+        for i in range(n):
+            for j in range(n):
+                if graph[i][j] == max_:
+                    print(i+1, max_)
+                    return
 
-n, e = map(int, input().split())
-graph = [[] for _ in range(n)]
-for _ in range(e):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-for u in range(n):
-    graph[u].sort()
-visited = [False] * n
-for u in range(n):
-    course = []
-    dfs(graph, visited, u, course)
-    print("{", *course, "}")
-visited = [False] * n
-for u in range(n):
-    course = []
-    bfs(graph, visited, u, course)
-    print("{", *course, "}")
+if __name__ == '__main__':
+    main()
+
+

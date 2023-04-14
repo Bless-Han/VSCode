afd94a6827123ace0b9eee4d5d6ea8593d52ce8e
Bless-Han <327496836@qq.com>
2023-04-14 11:09:37 +0800

    origin/main

--- solution/leetcode.py
+++ solution/leetcode.py
@@ -1,16 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        curr = head
-        while curr and curr.next:
-            if curr.next.val == curr.val:
-                curr.next = curr.next.next
-            else:
-                curr = curr.next
-        return head
+    def dominantIndex(self, nums: List[int]) -> int:
+        max_index = 0
+        max_other = 0
+        for i in range(len(nums)):
+            if nums[i] > nums[max_index]:
+                max_other = nums[max_index]
+                max_index = i
+        if max_other * 2 > nums[max_index]:
+            return -1
+        else:
+            return max_index
+

7f7811b42cd6ca50c29740b7d5fa66f5c8ccd94c
Bless-Han <327496836@qq.com>
2023-04-14 14:11:15 +0800

    origin/main

--- solution/leetcode.py
+++ solution/leetcode.py
@@ -1,13 +1,21 @@
 class Solution:
-    def dominantIndex(self, nums: List[int]) -> int:
-        max_index = 0
-        max_other = 0
-        for i in range(len(nums)):
-            if nums[i] > nums[max_index]:
-                max_other = nums[max_index]
-                max_index = i
-        if max_other * 2 > nums[max_index]:
-            return -1
-        else:
-            return max_index
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        ret = []
+        step = 0
+        for i in range(len(mat) * 2 - 1):
+            if step < len(mat):
+                left = 0
+                right = step
+                flag = 1
+            else:
+                left = 
+            if temp % 2 == 0:
+                left, right = right, left
+                flag = -flag
 
+            for _ in range(temp, -1, -1):
+                ret.append(mat[left][right])
+                left += flag
+                right -= flag
+            step += 1
+        return ret

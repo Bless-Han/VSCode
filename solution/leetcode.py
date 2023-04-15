a63ded7c16c2f1488b95265b08e3f7bde914b7c4
Bless-Han <327496836@qq.com>
2023-04-15 10:38:32 +0800

    origin/main

--- solution/leetcode.py
+++ solution/leetcode.py
@@ -5,10 +5,19 @@ class Solution:
         ret = []
         a = b = 0
         for i in range(len(mat) * 2 - 1):
+            ret.append(mat[a][b])
             if i < len(mat):
                 half = FONTHALF
             else:
                 half = ENDHALF
-            if i % 2 == 0:
+            if a == 0 or b == 0:
+                if a == 0 and half = FONTHALF:
+                    b += 1
+                elif a == 0 and half = ENDHALF:
+                    a += 1
+                elif b == 0 and half = FONTHALF:
+                    a += 1
+                elif b == 0 and half = ENDLAHF:
+
 
         return ret

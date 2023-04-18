e49ed63e3715d21832c7eab320e5e2c94df391b8
Bless-Han <327496836@qq.com>
2023-04-18 10:14:53 +0800

    origin/main

--- solution/leetcode.py
+++ solution/leetcode.py
@@ -1,14 +1,3 @@
 class Solution:
-    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        ans = {}
-        for divisor in divisors:
-            count = sum(1 for num in nums if num % divisor == 0)
-            ans[divisor] = count
-        max_divisor = divisors[0]
-        max_count = 0
-        print(ans)
-        for k, v in ans.items():
-            if v > max_count:
-                max_count = v
-                max_divisor = k
-        return max_divisor
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        

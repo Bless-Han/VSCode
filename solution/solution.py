0d296da56f3809f003a6507b9622e29d9bd89e9c
Bless-Han <327496836@qq.com>
2023-04-18 15:32:10 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,4 +1,18 @@
-a = [1, 3, 2, 3]
-while 3 in a:
-    a.remove(3)
-print(a)
+
+def bfs(graph, visited, u):
+    queue = (u, 0)
+    visited[u] = True
+    while queue:
+        v, depth = queue.pop(0)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for u in range(1, n+1):
+    visited = [False] * (n + 1)
+    bfs(graph, visited, u)
+    print(f"{u}: {sum(visited)*100/n:.2f}%")
+

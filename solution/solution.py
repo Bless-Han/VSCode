1ad14a9626a0ac138c4ff0b4f02f9d9beeb343dc
Bless-Han <327496836@qq.com>
2023-04-19 19:56:37 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,49 +1,5 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        RIGHT = 0
-        DOWN = 1
-        LEFT = 2
-        UP = 3
-        x = y = 0
-        state = RIGHT
-        x_len = len(matrix)
-        y_len = len(matrix[0])
-        visited = [[False] * y_len for _ in range(x_len)]
-        ans = []
-        while matrix[x][y] == False:
-            ans.append(matrix[x][y])
-            visited[x][y] = True
-            match state:
-                case RIGHT:
-                    if x + 1 == x_len or visited[x+1][y]:
-                        y += 1
-                        state = DOWN
-                    else:
-                        x += 1
-                case DOWN:
-                    if y + 1 == y_len or visited[x][y+1]:
-                        x -= 1
-                        state = LEFT
-                    else:
-                        y += 1
-                case LEFT:
-                    if x == 0 or visited[x-1][y]:
-                        y -= 1
-                        state = UP
-                    else:
-                        x -= 1
-                case UP:
-                    if visited[x][y-1]:
-                        x += 1
-                        state = RIGHT
-                    else:
-                        y -= 1
-        return ans
-
-RIGHT = 0
-DOWN = 1
-right = 0
-a = RIGHT
-match a:
-    case right: print("is 0")
-    case 1: print("is 1")
+a = list("abcd")
+b = a
+a[:] += ["abdef"]
+print(b)
+print(a)
--- test/temp.py
+++ test/temp.py
@@ -1,16 +1,6 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        if numRows == 1:
-            return [[1]]
-        if numRows == 2:
-            return [[1], [1, 1]]
-        result = [[1], [1, 1]]
-        for i in range(2, numRows):
-            temp = [1]
-            for j in range(1, i):
-                temp.append(result[i-1][j-1] + result[i-1][j])
-            temp.append(1)
-            result.append(temp)
-        return result
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i+1, j+1]

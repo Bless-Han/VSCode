734efc3d9661cbc6801542044fec174f263c82cc
Bless-Han <327496836@qq.com>
2023-04-20 19:24:09 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,29 +1,5 @@
-
-def floyd(graph, n):
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if graph[i][j] > graph[i][k] + graph[k][j]:
-                    graph[i][j] = graph[i][k] + graph[k][j]
-
-n, m = map(int, input().split())
-graph = [[float('inf')] * (n + 1) for _ in range(n+1)]
-for _ in range(m):
-    a, b, val = map(int, input().split())
-    graph[a][b] = graph[b][a] = val
-for i in range(1, n + 1):
-    graph[i][i] = 0
-floyd(graph, n)
-ans = float('inf')
-for i in range(1, n + 1):
-    max = 0
-    for j in range(1, n + 1):
-        if max < graph[i][j]:
-            max = graph[i][j]
-    if ans > max:
-        ans = max
-        x = i
-if ans == float('inf'):
-    print("0")
-else:
-    print(x, ans)
+a = set()
+a.add(1)
+a.add(3)
+a.remove(3)
+print(a)
--- test/temp.py
+++ test/temp.py
@@ -1,7 +1,9 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            n = sum(int(i) ** 2 for i in str(n))
+            if n in seen:
+                return False
+            seen.add(n)
+        return True

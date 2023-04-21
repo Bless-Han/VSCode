ae0f4770ba3fc39a377df97401eded711ed2d844
Bless-Han <327496836@qq.com>
2023-04-21 17:17:23 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -3,10 +3,6 @@ class Solution:
         d = {}
         for i in range(len(s)):
             if s[i] not in d:
-# Q: Why t[i] in d.values() then return False?
-# A: Because if t[i] in d.values(), it means that t[i] has been mapped to another s[i] before.
-# Q: I know now, because of the precondition "No two characters may map to the same character, but a character may map to itself." Thank you.
-# A: You are welcome.
                 if t[i] in d.values():
                     return False
                 d[s[i]] = t[i]

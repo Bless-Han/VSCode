7a416fb0b19f3456963ae531fe321b6b1aba7cb6
Bless-Han <327496836@qq.com>
2023-04-23 17:56:33 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,9 +1,2 @@
-students = [
-        {"name": "Ron", "house": "Gryffindor"},
-        {"name": "Draco", "house": "Slytherin"},
-        {"name": "Hermione", "house": "Gryffindor"},
-        {"name": "Harry", "house": "Gryffindor"},
-        ]
-
-for student in sorted(students, key=lambda x: x['name']):
-    print(student)
+a = ""
+b = "".join(sorted(a))

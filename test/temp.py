8a7ac60dbe2ad1902c3764ba993d894a8d3a2fc5
Bless-Han <327496836@qq.com>
2023-04-24 17:58:10 +0800

    origin/main

--- test/temp.py
+++ test/temp.py
@@ -1,15 +1,21 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        d = {}
-        for i, v in enumerate(nums):
-            if v not in d:
-                d[v] = i
-            else:
-                if i - d[v] <= k:
-                    return True
-                else:
-                    d[v] = i
-        return False
-
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+    def isValid(self, nums):
+        nums = [x for x in nums if x != '.']
+        return len(nums) == len(set(nums))
+            
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # check rows
+        for row in board:
+            if not self.isValid(row):
+                return False
+        # check columns
+        for col in zip(*board):
+            if not self.isValid(col):
+                return False
+        # check 3x3 squares
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.isValid(square):
+                    return False
+        return True

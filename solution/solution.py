055db45b0bc44c088139303436cf59232b4b97fd
Bless-Han <327496836@qq.com>
2023-04-25 11:15:53 +0800

    origin/main

--- solution/solution.py
+++ solution/solution.py
@@ -1,2 +1,8 @@
-a = "abcdefg"
-print(a[::-1])
+board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for col in zip(*board):
+    print(col)
--- test/temp.py
+++ test/temp.py
@@ -1,21 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isValid(self, nums):
-        nums = [x for x in nums if x != '.']
-        return len(nums) == len(set(nums))
-            
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # check rows
-        for row in board:
-            if not self.isValid(row):
-                return False
-        # check columns
-        for col in zip(*board):
-            if not self.isValid(col):
-                return False
-        # check 3x3 squares
-        for i in (0, 3, 6):
-            for j in (0, 3, 6):
-                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                if not self.isValid(square):
-                    return False
-        return True
+    
+
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        # Time Complexity: O(N)
+        # Space Complexity: O(N)
+
+        self.subtrees = {}
+        self.ret = []
+
+        self.traverse(root)
+        return self.ret

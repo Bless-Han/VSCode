b2f1d3d49261e4923557b5574e980fc6067f8929
Bless-Han <327496836@qq.com>
2024-11-12 08:04:09 +0800

    origin/main

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,7 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
 using namespace std;
 int main(){
-	#ifndef ONLINE_JUDGE
-	freopen("input", "r", stdin);
-	freopen("output", "w", stdout);
-	#endif
-
-	cout << "hell worold\n";
-	cout << "hell worold\n";
-	return 0;
+	printf("Hello world\n");
+	printf("Hello world\n");
 }

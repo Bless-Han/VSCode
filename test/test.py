a65fa009b868ff3a726475f8da4fc97c8ae8c03d
Bless-Han <327496836@qq.com>
2024-11-15 15:10:04 +0800

    origin/main

--- test/test.py
+++ test/test.py
@@ -1,15 +1,1 @@
-for x in range(5):
-    print('x: ', x)
-    try: print(pow(2.71828183, x))
-    except: print(None)
-    try: print(1 / (x * (x - 1) * (x - 2)))
-    except: print(None)
-    print('---------------------')
-
-x = 2.06
-print('x: ', x)
-try: print(pow(2.71828183, x))
-except: print(None)
-try: print(1 / (x * (x - 1) * (x - 2)))
-except: print(None)
-print('---------------------')
+print(pow(5, -2))
